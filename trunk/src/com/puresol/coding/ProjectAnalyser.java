aa5a857fc9d14b60fb238a934ffc8b0638754850
ludwig <ludwig@695a9040-d605-4429-820e-8508b0f4e340>
2009-12-21 21:56:26 +0000

    - added exception for invalid code
    git-svn-id: http://server1:10080/svn/PureSolTechnologiesAPI@33 695a9040-d605-4429-820e-8508b0f4e340

--- trunk/src/com/puresol/coding/ProjectAnalyser.java
+++ trunk/src/com/puresol/coding/ProjectAnalyser.java
@@ -8,94 +8,97 @@ import java.util.Set;
 
 import javax.i18n4j.FileSearch;
 
+import org.antlr.runtime.tree.RewriteEmptyStreamException;
 import org.apache.log4j.Logger;
 
 public class ProjectAnalyser {
 
-    private static final Logger logger =
-	    Logger.getLogger(ProjectAnalyser.class);
+	private static final Logger logger = Logger
+			.getLogger(ProjectAnalyser.class);
 
-    private File projectDirectory;
-    private String pattern;
-    private Hashtable<File, Analyser> analysers =
-	    new Hashtable<File, Analyser>();
+	private File projectDirectory;
+	private String pattern;
+	private Hashtable<File, Analyser> analysers = new Hashtable<File, Analyser>();
 
-    public ProjectAnalyser(File projectDirectory, String pattern) {
-	this.projectDirectory = projectDirectory;
-	this.pattern = pattern;
-    }
-
-    public void update() {
-	analysers = new Hashtable<File, Analyser>();
-	analyseFiles();
-	calculateStatistics();
-    }
-
-    private void analyseFiles() {
-	List<File> files =
-		FileSearch
-			.find(projectDirectory.getPath() + "/" + pattern);
-	for (File file : files) {
-	    analyseFile(file);
+	public ProjectAnalyser(File projectDirectory, String pattern) {
+		this.projectDirectory = projectDirectory;
+		this.pattern = pattern;
 	}
-    }
 
-    private void analyseFile(File file) {
-	try {
-	    if ((file.isFile()) && (!file.getPath().contains("/."))) {
-		String fileString =
-			file.getPath().toString().substring(
-				projectDirectory.getPath().length());
-		file = new File(fileString);
-		analysers.put(file, AnalyserFactory.createAnalyser(
-			projectDirectory, file));
-	    }
-	} catch (LanguageNotSupportedException e) {
-	    logger
-		    .warn("File '"
-			    + file.getPath()
-			    + "' could not be analysed due to containing no supported language.");
+	public void update() {
+		analysers = new Hashtable<File, Analyser>();
+		analyseFiles();
+		calculateStatistics();
 	}
-    }
 
-    private void calculateStatistics() {
-
-    }
-
-    public File getProjectDirectory() {
-	return projectDirectory;
-    }
-
-    public Set<File> getFiles() {
-	return analysers.keySet();
-    }
-
-    public Analyser getAnalyser(File file) {
-	return analysers.get(file);
-    }
-
-    public ArrayList<CodeRange> getCodeRanges(File file) {
-	if (analysers == null) {
-	    return null;
+	private void analyseFiles() {
+		List<File> files = FileSearch.find(projectDirectory.getPath() + "/"
+				+ pattern);
+		for (File file : files) {
+			analyseFile(file);
+		}
 	}
-	if (file == null) {
-	    return null;
-	}
-	if (!analysers.containsKey(file)) {
-	    return null;
-	}
-	Analyser analyser = analysers.get(file);
-	if (analyser == null) {
-	    return null;
-	}
-	return analysers.get(file).getCodeRanges();
-    }
 
-    public CodeRangeMetrics getMetrics(File file, CodeRange codeRange) {
-	Analyser analyser = analysers.get(file);
-	if (analyser == null) {
-	    return null;
+	private void analyseFile(File file) {
+		try {
+			if ((file.isFile()) && (!file.getPath().contains("/."))) {
+				String fileString = file.getPath().toString().substring(
+						projectDirectory.getPath().length());
+				file = new File(fileString);
+				analysers.put(file, AnalyserFactory.createAnalyser(
+						projectDirectory, file));
+			}
+		} catch (RewriteEmptyStreamException e) {
+			logger
+					.error("File '"
+							+ file.getPath()
+							+ "' could not be analysed due to coding error in source file!");
+		} catch (LanguageNotSupportedException e) {
+			logger
+					.warn("File '"
+							+ file.getPath()
+							+ "' could not be analysed due to containing no supported language.");
+		}
 	}
-	return analyser.getMetrics(codeRange);
-    }
+
+	private void calculateStatistics() {
+
+	}
+
+	public File getProjectDirectory() {
+		return projectDirectory;
+	}
+
+	public Set<File> getFiles() {
+		return analysers.keySet();
+	}
+
+	public Analyser getAnalyser(File file) {
+		return analysers.get(file);
+	}
+
+	public ArrayList<CodeRange> getCodeRanges(File file) {
+		if (analysers == null) {
+			return null;
+		}
+		if (file == null) {
+			return null;
+		}
+		if (!analysers.containsKey(file)) {
+			return null;
+		}
+		Analyser analyser = analysers.get(file);
+		if (analyser == null) {
+			return null;
+		}
+		return analysers.get(file).getCodeRanges();
+	}
+
+	public CodeRangeMetrics getMetrics(File file, CodeRange codeRange) {
+		Analyser analyser = analysers.get(file);
+		if (analyser == null) {
+			return null;
+		}
+		return analyser.getMetrics(codeRange);
+	}
 }
--- trunk/src/com/puresol/coding/java/antlr/output/JavaLexer.java
+++ trunk/src/com/puresol/coding/java/antlr/output/JavaLexer.java
@@ -4,9 +4,6 @@ package com.puresol.coding.java.antlr.output;
 
 
 import org.antlr.runtime.*;
-import java.util.Stack;
-import java.util.List;
-import java.util.ArrayList;
 
 public class JavaLexer extends Lexer {
     public static final int T__29=29;
