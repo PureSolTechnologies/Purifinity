c9f688111db764ff0680ad9864a82c5a3bccd038
ludwig <ludwig@695a9040-d605-4429-820e-8508b0f4e340>
2010-01-19 18:54:28 +0000

    - fixed Lexer, improvement didn't work out
    git-svn-id: http://server1:10080/svn/PureSolTechnologiesAPI@100 695a9040-d605-4429-820e-8508b0f4e340

--- trunk/src/com/puresol/parser/TokenStream.java
+++ trunk/src/com/puresol/parser/TokenStream.java
@@ -4,55 +4,50 @@ import java.util.ArrayList;
 
 public class TokenStream {
 
-    private final String name;
-    private final ArrayList<Token> tokens = new ArrayList<Token>();
+	private final String name;
+	private final ArrayList<Token> tokens = new ArrayList<Token>();
 
-    public TokenStream(String name) {
-	this.name = name;
-    }
-
-    public String getName() {
-	return name;
-    }
-
-    public ArrayList<Token> getTokens() {
-	return tokens;
-    }
-
-    public void addToken(Token token) {
-	tokens.add(token);
-    }
-
-    public void addTokenAtBegining(Token token) {
-	tokens.add(0, token);
-    }
-
-    public Token get(int index) {
-	return tokens.get(index);
-    }
-
-    public int getFirstVisbleTokenID() throws NoMatchingTokenException {
-	for (int index = 0; index < tokens.size(); index++) {
-	    if (get(index).getPublicity() != TokenPublicity.HIDDEN) {
-		return index;
-	    }
+	public TokenStream(String name) {
+		this.name = name;
 	}
-	throw new NoMatchingTokenException();
-    }
 
-    public int getSize() {
-	return tokens.size();
-    }
+	public String getName() {
+		return name;
+	}
 
-    public Token findPreviousToken(int tokenID)
-	    throws NoMatchingTokenException {
-	int position = tokenID - 1;
-	while (get(position).getPublicity() == TokenPublicity.HIDDEN) {
-	    if (position == 0) {
+	public ArrayList<Token> getTokens() {
+		return tokens;
+	}
+
+	public void addToken(Token token) {
+		tokens.add(token);
+	}
+
+	public Token get(int index) {
+		return tokens.get(index);
+	}
+
+	public int getFirstVisbleTokenID() throws NoMatchingTokenException {
+		for (int index = 0; index < tokens.size(); index++) {
+			if (get(index).getPublicity() != TokenPublicity.HIDDEN) {
+				return index;
+			}
+		}
 		throw new NoMatchingTokenException();
-	    }
-	    position--;
 	}
-	return get(position);
-    }
+
+	public int getSize() {
+		return tokens.size();
+	}
+
+	public Token findPreviousToken(int tokenID) throws NoMatchingTokenException {
+		int position = tokenID - 1;
+		while (get(position).getPublicity() == TokenPublicity.HIDDEN) {
+			if (position == 0) {
+				throw new NoMatchingTokenException();
+			}
+			position--;
+		}
+		return get(position);
+	}
 }
