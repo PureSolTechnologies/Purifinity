e64a7798bd201b4d88d19629b8c711a0badb2d6f
ludwig <ludwig@1c2ea5a9-06f0-4a16-8236-e230240cef1e>
2010-06-07 20:13:35 +0000

    - added support for unicode escapes
    git-svn-id: http://server1:10080/svn/JavaLanguagePack@11 1c2ea5a9-06f0-4a16-8236-e230240cef1e

--- trunk/test/com/puresol/coding/lang/java/samples/I18NRelease.java
+++ trunk/test/com/puresol/coding/lang/java/samples/I18NRelease.java
@@ -42,86 +42,87 @@ import org.apache.log4j.Logger;
  */
 public class I18NRelease {
 
-	private static final Logger logger = Logger.getLogger(I18NRelease.class);
-	private static final Translator translator = Translator
-			.getTranslator(I18NRelease.class);
+    private static final Logger logger = Logger.getLogger(I18NRelease.class);
+    private static final Translator translator = Translator
+	    .getTranslator(I18NRelease.class);
 
-	private String i18nDirectory = "";
-	private Vector<File> inputFiles = new Vector<File>();
+    private final File i18nDirectory;
+    private Vector<File> inputFiles = new Vector<File>();
 
-	public I18NRelease(String i18nDirectory) {
-		this.i18nDirectory = i18nDirectory;
+    public I18NRelease(File i18nDirectory) {
+	this.i18nDirectory = i18nDirectory;
+    }
+
+    public I18NRelease(String args[]) {
+	if ((args.length == 0) || (args.length > 1)) {
+	    this.i18nDirectory = null;
+	    showUsage();
+	    return;
 	}
+	this.i18nDirectory = new File(args[0]);
+    }
 
-	public I18NRelease(String args[]) {
-		if ((args.length == 0) || (args.length > 1)) {
-			showUsage();
-			return;
-		}
-		this.i18nDirectory = args[0];
-	}
+    private void showUsage() {
+	System.out.println("===========");
+	System.out.println("I18NRelease");
+	System.out.println("===========");
+	System.out.println();
+	System.out.println(translator.i18n("usage:  I18NRelease <directory>"));
+	System.out.println();
+	System.out
+		.println(translator
+			.i18n("This application converts all i18n files in the\n"
+				+ "the specified directory and converts them into\n"
+				+ "tr files for usage in internationalized applications."));
+    }
 
-	private void showUsage() {
-		System.out.println("===========");
-		System.out.println("I18NRelease");
-		System.out.println("===========");
-		System.out.println();
-		System.out.println(translator.i18n("usage:  I18NRelease <directory>"));
-		System.out.println();
-		System.out
-				.println(translator
-						.i18n("This application converts all i18n files in the\n"
-								+ "the specified directory and converts them into\n"
-								+ "tr files for usage in internationalized applications."));
-	}
+    public void release() {
+	findAllInputFiles();
+	processFiles();
+    }
 
-	public void release() {
-		findAllInputFiles();
-		processFiles();
-	}
+    private void findAllInputFiles() {
+	inputFiles.addAll(FileSearch.find(i18nDirectory, "**/*.i18n"));
+    }
 
-	private void findAllInputFiles() {
-		inputFiles.addAll(FileSearch.find(i18nDirectory + "/**/*.i18n"));
+    private void processFiles() {
+	Iterator<File> i = inputFiles.iterator();
+	while (i.hasNext()) {
+	    File file = i.next();
+	    processFile(file);
 	}
+    }
 
-	private void processFiles() {
-		Iterator<File> i = inputFiles.iterator();
-		while (i.hasNext()) {
-			File file = i.next();
-			processFile(file);
-		}
+    private void processFile(File file) {
+	try {
+	    logger.info(translator.i18n("Process file {0}...", file.getPath()));
+	    MultiLanguageTranslations mlTranslations = I18NFile.read(file);
+	    Set<String> languages = mlTranslations.getAvailableLanguages();
+	    for (String language : languages) {
+		release(mlTranslations, language, new File(file.getPath()
+			.replaceAll("\\.i18n", "." + language + ".tr")));
+	    }
+	} catch (FileNotFoundException e) {
+	    logger.error(e.getMessage(), e);
 	}
+    }
 
-	private void processFile(File file) {
-		try {
-			logger.info(translator.i18n("Process file {0}...", file.getPath()));
-			MultiLanguageTranslations mlTranslations = I18NFile.read(file);
-			Set<String> languages = mlTranslations.getAvailableLanguages();
-			for (String language : languages) {
-				release(mlTranslations, language, new File(file.getPath()
-						.replaceAll("\\.i18n", "." + language + ".tr")));
-			}
-		} catch (FileNotFoundException e) {
-			logger.error(e.getMessage(), e);
-		}
+    private void release(MultiLanguageTranslations mlTranslations,
+	    String language, File file) {
+	logger.info(translator.i18n("Release language {0}  to file {1}",
+		language, file.getPath()));
+	SingleLanguageTranslations translations = new SingleLanguageTranslations();
+	Set<String> sources = mlTranslations.getSources();
+	for (String source : sources) {
+	    LanguageSet set = mlTranslations.get(source);
+	    if (set.containsLanguage(language)) {
+		translations.set(source, set.get(language));
+	    }
 	}
+	TRFile.write(file, translations);
+    }
 
-	private void release(MultiLanguageTranslations mlTranslations,
-			String language, File file) {
-		logger.info(translator.i18n("Release language {0}  to file {1}",
-				language, file.getPath()));
-		SingleLanguageTranslations translations = new SingleLanguageTranslations();
-		Set<String> sources = mlTranslations.getSources();
-		for (String source : sources) {
-			LanguageSet set = mlTranslations.get(source);
-			if (set.containsLanguage(language)) {
-				translations.set(source, set.get(language));
-			}
-		}
-		TRFile.write(file, translations);
-	}
-
-	public static void main(String[] args) {
-		new I18NRelease(args).release();
-	}
+    public static void main(String[] args) {
+	new I18NRelease(args).release();
+    }
 }
