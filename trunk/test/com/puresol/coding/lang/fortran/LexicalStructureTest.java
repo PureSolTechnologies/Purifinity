2d679d8c5dee4779df84016d8e9b64435a4ca2e6
ludwig <ludwig@d48d4e16-8ee7-4fa7-9cc4-74feb2bffba7>
2010-06-08 21:25:04 +0000

    - implementation work
    git-svn-id: http://server1:10080/svn/FortranLanguagePack@5 d48d4e16-8ee7-4fa7-9cc4-74feb2bffba7

--- trunk/test/com/puresol/coding/lang/fortran/LexicalStructureTest.java
+++ trunk/test/com/puresol/coding/lang/fortran/LexicalStructureTest.java
@@ -8,15 +8,33 @@ import junit.framework.TestCase;
 
 public class LexicalStructureTest extends TestCase {
 
-    @Test
-    public void testSignificant() {
-	Pattern.matches(LexicalStructure.SIGNIFICANT, ".12345");
-	Pattern.matches(LexicalStructure.SIGNIFICANT, "12345.");
-	Pattern.matches(LexicalStructure.SIGNIFICANT, "12345.12345");
-    }
+	@Test
+	public void testSignificant() {
+		Pattern.matches(LexicalStructure.SIGNIFICANT, ".12345");
+		Pattern.matches(LexicalStructure.SIGNIFICANT, "12345.");
+		Pattern.matches(LexicalStructure.SIGNIFICANT, "12345.12345");
+	}
 
-    @Test
-    public void testRealLiteralConstant() {
-	// TODO
-    }
+	@Test
+	public void testRealLiteralConstant() {
+		Pattern.matches(LexicalStructure.REAL_LITERAL_CONSTANT, "12345.12345");
+		Pattern.matches(LexicalStructure.REAL_LITERAL_CONSTANT,
+				"12345.12345E+1");
+		Pattern.matches(LexicalStructure.REAL_LITERAL_CONSTANT,
+				"12345.12345e-1");
+		Pattern.matches(LexicalStructure.REAL_LITERAL_CONSTANT, "1e-1");
+		Pattern.matches(LexicalStructure.REAL_LITERAL_CONSTANT, "1E+1");
+	}
+
+	@Test
+	public void testSignedRealLiteralConstant() {
+		Pattern.matches(LexicalStructure.SIGNED_REAL_LITERAL_CONSTANT,
+				"+12345.12345");
+		Pattern.matches(LexicalStructure.SIGNED_REAL_LITERAL_CONSTANT,
+				"-12345.12345E+1");
+		Pattern.matches(LexicalStructure.SIGNED_REAL_LITERAL_CONSTANT,
+				"+12345.12345e-1");
+		Pattern.matches(LexicalStructure.SIGNED_REAL_LITERAL_CONSTANT, "-1e-1");
+		Pattern.matches(LexicalStructure.SIGNED_REAL_LITERAL_CONSTANT, "1E+1");
+	}
 }
