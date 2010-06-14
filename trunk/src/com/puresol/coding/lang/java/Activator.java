6c33c4af3b13387d293d863be1c819ea296c2bde
ludwig <ludwig@1c2ea5a9-06f0-4a16-8236-e230240cef1e>
2010-06-14 19:56:49 +0000

    - updates in naming and refactoring updates
    git-svn-id: http://server1:10080/svn/JavaLanguagePack@19 1c2ea5a9-06f0-4a16-8236-e230240cef1e

--- trunk/src/com/puresol/coding/lang/java/Activator.java
+++ trunk/src/com/puresol/coding/lang/java/Activator.java
@@ -15,21 +15,21 @@ import com.puresol.coding.ProgrammingLanguages;
  */
 public class Activator implements BundleActivator {
 
-    private static final Logger logger = Logger.getLogger(Activator.class);
+	private static final Logger logger = Logger.getLogger(Activator.class);
 
-    @Override
-    public void start(BundleContext context) throws Exception {
-	logger.info("Starting Java Language Plugin...");
-	ProgrammingLanguages.getInstance().registerLanguage(Java.getInstance());
-	logger.info("Started.");
-    }
+	@Override
+	public void start(BundleContext context) throws Exception {
+		logger.info("Starting Java Language Pack...");
+		ProgrammingLanguages.getInstance().registerLanguage(Java.getInstance());
+		logger.info("Started.");
+	}
 
-    @Override
-    public void stop(BundleContext context) throws Exception {
-	logger.info("Stopping Java Language Plugin...");
-	ProgrammingLanguages.getInstance().unregisterLanguage(
-		Java.getInstance());
-	logger.info("Stopped.");
-    }
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		logger.info("Stopping Java Language Pack...");
+		ProgrammingLanguages.getInstance().unregisterLanguage(
+				Java.getInstance());
+		logger.info("Stopped.");
+	}
 
 }
