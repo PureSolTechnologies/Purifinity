f1f7ba22e362d87ea75236f8435c739b28cced10
ludwig <ludwig@d48d4e16-8ee7-4fa7-9cc4-74feb2bffba7>
2010-08-07 14:21:41 +0000

    - removed goto evaluators due to need of new concept
    git-svn-id: http://server1:10080/svn/FortranLanguagePack@30 d48d4e16-8ee7-4fa7-9cc4-74feb2bffba7

--- trunk/test/com/puresol/coding/lang/fortran/FortranLexerTest.java
+++ trunk/test/com/puresol/coding/lang/fortran/FortranLexerTest.java
@@ -8,10 +8,7 @@ import junit.framework.Assert;
 
 import org.junit.Test;
 
-import com.puresol.coding.analysis.Analyzer;
-import com.puresol.coding.analysis.ProjectAnalyzer;
 import com.puresol.coding.analysis.SourceCodeLexer;
-import com.puresol.coding.lang.fortran.evaluator.GotoEvaluator;
 import com.puresol.parser.LexerException;
 import com.puresol.parser.NoMatchingTokenDefinitionFound;
 import com.puresol.parser.Token;
@@ -38,37 +35,6 @@ public class FortranLexerTest {
 		} catch (IOException e) {
 			e.printStackTrace();
 			Assert.fail("No exception was expected!");
-		} catch (NoMatchingTokenDefinitionFound e) {
-			e.printStackTrace();
-			Assert.fail("No exception was expected!");
-		} catch (LexerException e) {
-			e.printStackTrace();
-			Assert.fail("No exception was expected!");
-		}
-	}
-
-	public static void main(String args[]) {
-		try {
-			ProjectAnalyzer projectAnalyser = ProjectAnalyzer.create(new File(
-					"fort/dyn3d_3.f"), new File("../Dyn3D/src"));
-			projectAnalyser.run();
-			Assert.assertTrue(projectAnalyser.getFiles().size() > 0);
-			for (File file : projectAnalyser.getFiles()) {
-				System.out.println(file + "\n");
-			}
-			Analyzer analyser = projectAnalyser.getAnalyzer(new File(
-					"/fort/dyn3d_3.f"));
-			TokenStream tokenStream = analyser.getRootCodeRange()
-					.getTokenStream();
-			SourceCodeLexer lexer = new SourceCodeLexer(Fortran.getInstance(),
-					tokenStream);
-			TokenStream tokenStream2 = lexer.getTokenStream();
-			for (Token token : tokenStream2.getTokens()) {
-				System.out.println(token.toString());
-			}
-			GotoEvaluator evaluator = new GotoEvaluator(projectAnalyser);
-			evaluator.run();
-			Assert.assertTrue(evaluator.getGotoNum() > 0);
 		} catch (NoMatchingTokenDefinitionFound e) {
 			e.printStackTrace();
 			Assert.fail("No exception was expected!");
