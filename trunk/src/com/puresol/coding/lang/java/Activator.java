47355dbb2e579572f25b1c90a8f0055d6418e49f
ludwig <ludwig@1c2ea5a9-06f0-4a16-8236-e230240cef1e>
2010-11-15 20:44:09 +0000

    - fixed errors
    git-svn-id: http://server1:10080/svn/JavaLanguagePack@49 1c2ea5a9-06f0-4a16-8236-e230240cef1e

--- trunk/src/com/puresol/coding/lang/java/Activator.java
+++ trunk/src/com/puresol/coding/lang/java/Activator.java
@@ -5,7 +5,6 @@ import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
 import com.puresol.coding.ProgrammingLanguages;
-import com.puresol.coding.evaluator.Evaluators;
 
 /**
  * This class is used as OSGi bundle activator. This class only registers and
@@ -18,24 +17,16 @@ public class Activator implements BundleActivator {
 
 	private static final Logger logger = Logger.getLogger(Activator.class);
 
-	private TranslatorImplementationFactory translatorImplementationFactory = null;
-
 	@Override
 	public void start(BundleContext context) throws Exception {
 		logger.info("Starting Java Language Pack...");
 		ProgrammingLanguages.getInstance().registerLanguage(Java.getInstance());
-		translatorImplementationFactory = new TranslatorImplementationFactory();
-		Evaluators.getInstance().registerFileEvaluator(
-				translatorImplementationFactory);
 		logger.info("Started.");
 	}
 
 	@Override
 	public void stop(BundleContext context) throws Exception {
 		logger.info("Stopping Java Language Pack...");
-		Evaluators.getInstance().unregisterFileEvaluator(
-				translatorImplementationFactory);
-		translatorImplementationFactory = null;
 		ProgrammingLanguages.getInstance().unregisterLanguage(
 				Java.getInstance());
 		logger.info("Stopped.");
