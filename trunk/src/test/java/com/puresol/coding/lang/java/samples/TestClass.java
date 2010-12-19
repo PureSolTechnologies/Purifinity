f4a57c8e45b0380c51015ad416f0952fbe498ca2
ludwig <ludwig@1c2ea5a9-06f0-4a16-8236-e230240cef1e>
2010-12-19 21:40:48 +0000

    - added deployment description for Maven
    git-svn-id: http://server1:10080/svn/JavaLanguagePack@70 1c2ea5a9-06f0-4a16-8236-e230240cef1e

--- trunk/src/test/java/com/puresol/coding/lang/java/samples/TestClass.java
+++ trunk/src/test/java/com/puresol/coding/lang/java/samples/TestClass.java
@@ -1,22 +1,22 @@
 package com.puresol.coding.lang.java.samples;
 
+import static org.junit.Assert.*;
+
 import java.io.IOException;
 import java.io.Serializable;
 
-public class Test implements Serializable {
+import org.junit.Test;
+
+public class TestClass implements Serializable {
 
 	private static final long serialVersionUID = 2921265612698155191L;
 
 	private final int a;
 	private final double c = 10.1;
 
-	public Test() {
-		a = 0;
-	}
-
-	public Test(int b) {
+	public TestClass() {
 		super();
-		a = b;
+		a = 0;
 	}
 
 	public void publicMethod(int c) throws IOException {
@@ -29,5 +29,10 @@ public class Test implements Serializable {
 
 	public double getC() {
 		return c;
+	}
+
+	@Test
+	public void test() {
+		assertNotNull(new TestClass());
 	}
 }
