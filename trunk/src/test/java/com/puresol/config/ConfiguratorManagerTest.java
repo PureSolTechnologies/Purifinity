c1919f0f6f7ca9901a01a27aa438f7209db01239
ludwig <ludwig@745a9be5-5bbc-4d5d-875f-099a5ef90407>
2011-02-28 21:57:43 +0000

    - updated Maven project
    git-svn-id: http://server1:10080/svn/PureConfigurator@3 745a9be5-5bbc-4d5d-875f-099a5ef90407

--- trunk/src/test/java/com/puresol/config/ConfiguratorManagerTest.java
+++ trunk/src/test/java/com/puresol/config/ConfiguratorManagerTest.java
@@ -1,5 +1,14 @@
 package com.puresol.config;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 public class ConfiguratorManagerTest {
+
+	@Test
+	public void testInstance() {
+		assertNotNull(new ConfigurationManager());
+	}
 
 }
