076bfbd2f20c25c97cca9a4801cca1a0e12223e3
ludwig <ludwig@9ff23e7d-eca8-44d0-b5e4-b0a0aa7255e4>
2011-07-06 21:30:27 +0000

    - added first Scala source for checking Scala environment - added thesis by Ford about Packrat Parsing
    git-svn-id: http://server1:10080/svn/NyotaUhura@133 9ff23e7d-eca8-44d0-b5e4-b0a0aa7255e4

---
+++ trunk/src/main/scala/com/puresol/uhura/parser/packrat/TestApplication.scala
@@ -0,0 +1,9 @@
+package com.puresol.uhura.parser.packrat;
+
+object TestApplication {
+  
+  def main(args: Array[String]) {
+    println("Test")
+  }
+  
+}
---
+++ trunk/src/test/scala/com/puresol/uhura/parser/packrat/PackratParserTest.scala
@@ -0,0 +1,11 @@
+package com.puresol.uhura.parser.packrat
+
+import org.junit.Test
+
+class PackratParserTest {
+  
+  @Test
+  def testInstance() = {
+  }
+
+}
