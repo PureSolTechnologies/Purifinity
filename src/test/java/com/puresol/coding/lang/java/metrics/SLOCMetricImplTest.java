9400110f152678a95bf62d95db40854313c39914
Rick-Rainer Ludwig <ludwig@puresol-technologies.com>
2011-12-22 22:10:40 +0100

    Minor change.

--- src/test/java/com/puresol/coding/lang/java/metrics/SLOCMetricImplTest.java
+++ src/test/java/com/puresol/coding/lang/java/metrics/SLOCMetricImplTest.java
@@ -1,9 +1,11 @@
 package com.puresol.coding.lang.java.metrics;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.List;
 
 import org.junit.Test;
@@ -18,38 +20,35 @@ import com.puresol.utils.FileUtilities;
 
 public class SLOCMetricImplTest {
 
-	@Test
-	public void test() {
-		try {
-			Java java = Java.getInstance();
-			assertNotNull(java);
-			File file = new File("src/main/java", FileUtilities
-					.classToRelativePackagePath(SLOCMetricImpl.class).getPath());
-			assertTrue(file.exists());
-			Analyzer analyzer = java.createAnalyser(file);
-			assertNotNull(analyzer);
-			analyzer.parse();
-			List<CodeRange> codeRanges = analyzer.getAnalyzableCodeRanges();
-			assertNotNull(codeRanges);
-			assertTrue(codeRanges.size() > 0);
-			SLOCMetric metric = new SLOCMetric(java, codeRanges.get(2));
-			metric.run();
-			SLOCResult sloc = metric.getResult();
-			System.out.println("phyLOC: " + sloc.getPhyLOC());
-			System.out.println("proLOC: " + sloc.getProLOC());
-			System.out.println("comLOC: " + sloc.getComLOC());
-			System.out.println("blLOC: " + sloc.getBlLOC());
-			System.out.println("line length: " + sloc.getLineStatistics());
-			assertEquals(15, sloc.getPhyLOC());
-			assertEquals(13, sloc.getProLOC());
-			assertEquals(0, sloc.getComLOC());
-			assertEquals(2, sloc.getBlLOC());
-		} catch (AnalyzerException e) {
-			e.printStackTrace();
-			fail("No exception was expected!");
-		} catch (IOException e) {
-			e.printStackTrace();
-			fail("No exception was expected!");
-		}
+    @Test
+    public void test() {
+	try {
+	    Java java = Java.getInstance();
+	    assertNotNull(java);
+	    File file = new File("src/main/java", FileUtilities
+		    .classToRelativePackagePath(SLOCMetricImpl.class).getPath());
+	    assertTrue(file.exists());
+	    Analyzer analyzer = java.createAnalyser(file);
+	    assertNotNull(analyzer);
+	    analyzer.parse();
+	    List<CodeRange> codeRanges = analyzer.getAnalyzableCodeRanges();
+	    assertNotNull(codeRanges);
+	    assertTrue(codeRanges.size() > 0);
+	    SLOCMetric metric = new SLOCMetric(java, codeRanges.get(2));
+	    metric.run();
+	    SLOCResult sloc = metric.getResult();
+	    System.out.println("phyLOC: " + sloc.getPhyLOC());
+	    System.out.println("proLOC: " + sloc.getProLOC());
+	    System.out.println("comLOC: " + sloc.getComLOC());
+	    System.out.println("blLOC: " + sloc.getBlLOC());
+	    System.out.println("line length: " + sloc.getLineStatistics());
+	    assertEquals(15, sloc.getPhyLOC());
+	    assertEquals(13, sloc.getProLOC());
+	    assertEquals(0, sloc.getComLOC());
+	    assertEquals(2, sloc.getBlLOC());
+	} catch (AnalyzerException e) {
+	    e.printStackTrace();
+	    fail("No exception was expected!");
 	}
+    }
 }
