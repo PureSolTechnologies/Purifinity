6961462790cbb54325ba4e72db8f3bc5429487e4
Rick-Rainer Ludwig <ludwig@puresol-technologies.com>
2011-12-27 14:31:29 +0100

    Added PureSolDialog class and default dialog buttons support.

--- src/main/java/com/puresol/gui/osgi/BundleConfigurationDialog.java
+++ src/main/java/com/puresol/gui/osgi/BundleConfigurationDialog.java
@@ -1,15 +1,11 @@
 package com.puresol.gui.osgi;
 
 import java.awt.BorderLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.Container;
 
 import javax.i18n4java.Translator;
 import javax.swing.BorderFactory;
-import javax.swing.JButton;
-import javax.swing.JDialog;
 import javax.swing.JOptionPane;
-import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
 import javax.swing.event.TreeSelectionEvent;
@@ -17,6 +13,8 @@ import javax.swing.event.TreeSelectionListener;
 import javax.swing.tree.TreePath;
 
 import com.puresol.gui.Application;
+import com.puresol.gui.DialogButtons;
+import com.puresol.gui.PureSolDialog;
 import com.puresol.osgi.BundleConfigurator;
 
 /**
@@ -25,76 +23,68 @@ import com.puresol.osgi.BundleConfigurator;
  * @author Rick-Rainer Ludwig
  * 
  */
-public class BundleConfigurationDialog extends JDialog implements
-		TreeSelectionListener, ActionListener {
+public class BundleConfigurationDialog extends PureSolDialog implements
+	TreeSelectionListener {
 
-	private static final long serialVersionUID = -359245614681727468L;
+    private static final long serialVersionUID = -359245614681727468L;
 
-	private static final Translator translator = Translator
-			.getTranslator(BundleConfigurationDialog.class);
+    private static final Translator translator = Translator
+	    .getTranslator(BundleConfigurationDialog.class);
 
-	private final String frameworkName;
-	private final BundleConfiguratorTreeModel configuratorTreeView = new BundleConfiguratorTreeModel();
-	private final BundleConfiguratorPanel configPanel = new BundleConfiguratorPanel();
-	private final JButton closeButton = new JButton(translator.i18n("Close"));
+    private final String frameworkName;
+    private final BundleConfiguratorTreeModel configuratorTreeView = new BundleConfiguratorTreeModel();
+    private final BundleConfiguratorPanel configPanel = new BundleConfiguratorPanel();
 
-	public BundleConfigurationDialog(String frameworkName) {
-		super(Application.getInstance(), translator
-				.i18n("Plugin Configurations Dialog"), true);
-		this.frameworkName = frameworkName;
-		initUI();
+    public BundleConfigurationDialog(String frameworkName) {
+	super(Application.getInstance(), translator
+		.i18n("Plugin Configurations Dialog"), true);
+	this.frameworkName = frameworkName;
+	setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+	initUI();
+    }
+
+    private void initUI() {
+	Container contentPane = getContentPane();
+
+	configuratorTreeView.setConfiguratorTree(BundleConfiguratorTree
+		.create(frameworkName));
+	configuratorTreeView.setBorder(BorderFactory
+		.createTitledBorder(translator.i18n("Configurator")));
+	configuratorTreeView.addTreeSelectionListener(this);
+
+	contentPane.add(new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,
+		new JScrollPane(configuratorTreeView), configPanel),
+		BorderLayout.CENTER);
+
+	setButtonVisible(DialogButtons.CLOSE, true);
+
+	pack();
+    }
+
+    private void changeConfigurator(TreePath treePath) {
+	final BundleConfiguratorTree tree = (BundleConfiguratorTree) treePath
+		.getPathComponent(treePath.getPathCount() - 1);
+	if (tree != null) {
+	    BundleConfigurator configurator = tree.getConfigurator();
+	    configPanel.setBundleConfigurator(configurator);
 	}
+    }
 
-	private void initUI() {
-		JPanel contentPanel = new JPanel();
-		setContentPane(contentPanel);
-		contentPanel.setLayout(new BorderLayout());
+    @Override
+    public void close() {
+	JOptionPane
+		.showMessageDialog(
+			Application.getInstance(),
+			translator
+				.i18n("Closeing does not perform checks\nfor changed values and does not save them!"),
+			translator.i18n("Warning"), JOptionPane.WARNING_MESSAGE);
+	super.close();
+    }
 
-		configuratorTreeView.setConfiguratorTree(BundleConfiguratorTree
-				.create(frameworkName));
-		configuratorTreeView.setBorder(BorderFactory
-				.createTitledBorder(translator.i18n("Configurator")));
-		configuratorTreeView.addTreeSelectionListener(this);
-
-		contentPanel.add(new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,
-				new JScrollPane(configuratorTreeView), configPanel),
-				BorderLayout.CENTER);
-
-		closeButton.addActionListener(this);
-		contentPanel.add(closeButton, BorderLayout.SOUTH);
-		pack();
+    @Override
+    public void valueChanged(TreeSelectionEvent e) {
+	if (e.getSource() == configuratorTreeView) {
+	    changeConfigurator(e.getPath());
 	}
-
-	private void changeConfigurator(TreePath treePath) {
-		final BundleConfiguratorTree tree = (BundleConfiguratorTree) treePath
-				.getPathComponent(treePath.getPathCount() - 1);
-		if (tree != null) {
-			BundleConfigurator configurator = tree.getConfigurator();
-			configPanel.setBundleConfigurator(configurator);
-		}
-	}
-
-	private void close() {
-		JOptionPane
-				.showMessageDialog(
-						Application.getInstance(),
-						translator
-								.i18n("Closeing does not perform checks\nfor changed values and does not save them!"),
-						translator.i18n("Warning"), JOptionPane.WARNING_MESSAGE);
-		dispose();
-	}
-
-	@Override
-	public void valueChanged(TreeSelectionEvent e) {
-		if (e.getSource() == configuratorTreeView) {
-			changeConfigurator(e.getPath());
-		}
-	}
-
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		if (e.getSource() == closeButton) {
-			close();
-		}
-	}
+    }
 }
--- src/main/java/com/puresol/gui/osgi/BundleConfiguratorPanel.java
+++ src/main/java/com/puresol/gui/osgi/BundleConfiguratorPanel.java
@@ -17,94 +17,95 @@ import com.puresol.osgi.BundleConfigurator;
 
 public class BundleConfiguratorPanel extends JPanel implements ActionListener {
 
-	private static final long serialVersionUID = -5497942963691954365L;
+    private static final long serialVersionUID = -5497942963691954365L;
 
-	private static final Translator translator = Translator
-			.getTranslator(BundleConfiguratorPanel.class);
+    private static final Translator translator = Translator
+	    .getTranslator(BundleConfiguratorPanel.class);
 
-	private final PropertiesPanel propertiesPanel = new PropertiesPanel();
-	private final JLabel nameLabel = new JLabel();
-	private final JButton okButton = new JButton(translator.i18n("OK"));
-	private final JButton applyButton = new JButton(translator.i18n("Apply"));
-	private final JButton resetButton = new JButton(translator.i18n("Reset"));
-	private final JButton cancelButton = new JButton(translator.i18n("Cancel"));
+    private final PropertiesPanel propertiesPanel = new PropertiesPanel();
+    private final JLabel nameLabel = new JLabel();
+    private final JButton okButton = new JButton(translator.i18n("OK"));
+    private final JButton applyButton = new JButton(translator.i18n("Apply"));
+    private final JButton resetButton = new JButton(translator.i18n("Reset"));
+    private final JButton cancelButton = new JButton(translator.i18n("Cancel"));
 
-	public BundleConfiguratorPanel() {
-		super();
-		initUI();
+    public BundleConfiguratorPanel() {
+	super();
+	initUI();
+    }
+
+    public BundleConfiguratorPanel(BundleConfigurator bundleConfigurator) {
+	super();
+	initUI();
+	setBundleConfigurator(bundleConfigurator);
+    }
+
+    private void initUI() {
+	setLayout(new BorderLayout());
+
+	Font font = nameLabel.getFont();
+	nameLabel.setFont(new Font(font.getName(), font.getStyle(), (int) (font
+		.getSize() * 1.5)));
+	add(nameLabel, BorderLayout.NORTH);
+
+	add(new JScrollPane(propertiesPanel), BorderLayout.CENTER);
+
+	JPanel buttonsPanel = new JPanel();
+	buttonsPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+
+	okButton.addActionListener(this);
+	buttonsPanel.add(okButton);
+
+	applyButton.addActionListener(this);
+	buttonsPanel.add(applyButton);
+
+	resetButton.addActionListener(this);
+	buttonsPanel.add(resetButton);
+
+	cancelButton.addActionListener(this);
+	buttonsPanel.add(cancelButton);
+	add(buttonsPanel, BorderLayout.SOUTH);
+    }
+
+    public void setBundleConfigurator(BundleConfigurator bundleConfigurator) {
+	if (bundleConfigurator == null) {
+	    nameLabel.setText(translator.i18n("Nothing selected..."));
+	    propertiesPanel.clearPropertyDescriptions();
+	} else {
+	    nameLabel.setText(bundleConfigurator.getName());
+	    propertiesPanel.setPropertyDescriptions(
+		    bundleConfigurator.getSource(),
+		    bundleConfigurator.getPropertyDescriptions());
 	}
+    }
 
-	public BundleConfiguratorPanel(BundleConfigurator bundleConfigurator) {
-		super();
-		initUI();
-		setBundleConfigurator(bundleConfigurator);
+    private void ok() {
+	propertiesPanel.apply();
+	setBundleConfigurator(null);
+    }
+
+    private void apply() {
+	propertiesPanel.apply();
+    }
+
+    private void reset() {
+	propertiesPanel.reset();
+    }
+
+    private void cancel() {
+	setBundleConfigurator(null);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+	if (e.getSource() == okButton) {
+	    ok();
+	} else if (e.getSource() == applyButton) {
+	    apply();
+	} else if (e.getSource() == resetButton) {
+	    reset();
+	} else if (e.getSource() == cancelButton) {
+	    cancel();
 	}
-
-	private void initUI() {
-		setLayout(new BorderLayout());
-
-		Font font = nameLabel.getFont();
-		nameLabel.setFont(new Font(font.getName(), font.getStyle(), (int) (font
-				.getSize() * 1.5)));
-		add(nameLabel, BorderLayout.NORTH);
-
-		add(new JScrollPane(propertiesPanel), BorderLayout.CENTER);
-
-		JPanel buttonsPanel = new JPanel();
-		buttonsPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
-
-		okButton.addActionListener(this);
-		buttonsPanel.add(okButton);
-
-		applyButton.addActionListener(this);
-		buttonsPanel.add(applyButton);
-
-		resetButton.addActionListener(this);
-		buttonsPanel.add(resetButton);
-
-		cancelButton.addActionListener(this);
-		buttonsPanel.add(cancelButton);
-		add(buttonsPanel, BorderLayout.SOUTH);
-	}
-
-	public void setBundleConfigurator(BundleConfigurator bundleConfigurator) {
-		if (bundleConfigurator == null) {
-			nameLabel.setText(translator.i18n("Nothing selected..."));
-			propertiesPanel.clearPropertyDescriptions();
-		} else {
-			nameLabel.setText(bundleConfigurator.getName());
-			propertiesPanel.setPropertyDescriptions(bundleConfigurator
-					.getSource(), bundleConfigurator.getPropertyDescriptions());
-		}
-	}
-
-	private void ok() {
-		propertiesPanel.apply();
-		setBundleConfigurator(null);
-	}
-
-	private void apply() {
-		propertiesPanel.apply();
-	}
-
-	private void reset() {
-		propertiesPanel.reset();
-	}
-
-	private void cancel() {
-		setBundleConfigurator(null);
-	}
-
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		if (e.getSource() == okButton) {
-			ok();
-		} else if (e.getSource() == applyButton) {
-			apply();
-		} else if (e.getSource() == resetButton) {
-			reset();
-		} else if (e.getSource() == cancelButton) {
-			cancel();
-		}
-	}
+    }
 }
