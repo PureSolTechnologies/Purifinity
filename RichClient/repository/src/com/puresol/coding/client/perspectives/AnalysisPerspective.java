a2be843acb8f99d47d09dbe124fc918cac13d5b7
Rick-Rainer Ludwig <ludwig@puresol-technologies.com>
2012-01-07 00:17:48 +0100

    Added some more templates for views.

--- RichClient/repository/src/com/puresol/coding/client/perspectives/AnalysisPerspective.java
+++ RichClient/repository/src/com/puresol/coding/client/perspectives/AnalysisPerspective.java
@@ -13,5 +13,7 @@ public class AnalysisPerspective implements IPerspectiveFactory {
 		layout.getEditorArea());
 	layout.addView(ViewIds.SOURCE_NAVIGATOR, IPageLayout.BOTTOM, 0.5f,
 		ViewIds.ANALYSIS_NAVIGATOR);
+	layout.addView(ViewIds.ANALYSIS_REPORT, IPageLayout.BOTTOM, 0.8f,
+		layout.getEditorArea());
     }
 }
--- RichClient/repository/src/com/puresol/coding/client/perspectives/EvaluationPerspective.java
+++ RichClient/repository/src/com/puresol/coding/client/perspectives/EvaluationPerspective.java
@@ -3,12 +3,14 @@ package com.puresol.coding.client.perspectives;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
 
+import com.puresol.coding.client.views.ViewIds;
+
 public class EvaluationPerspective implements IPerspectiveFactory {
 
     @Override
     public void createInitialLayout(IPageLayout layout) {
-	// TODO Auto-generated method stub
-
+	layout.addView(ViewIds.ANALYSIS_REPORT, IPageLayout.BOTTOM, 0.8f,
+		layout.getEditorArea());
     }
 
 }
