9b42e59f5925d4d8a22b60a1a962b192e3acb474
Rick-Rainer Ludwig <ludwig@puresol-technologies.com>
2012-01-15 00:33:21 +0100

    By adding an empty directory errors can be removed about missing directories which are exported and the external non OSGi libraries need to be added as external resources to get the Eclipse projects compiling within Eclipse.

--- rcp/plugin/src/com/puresol/coding/client/wizards/NewAnalysisStartPage.java
+++ rcp/plugin/src/com/puresol/coding/client/wizards/NewAnalysisStartPage.java
@@ -1,15 +1,21 @@
 package com.puresol.coding.client.wizards;
 
+import javax.i18n4java.Translator;
+
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 
 public class NewAnalysisStartPage extends WizardPage {
 
+    private static final Translator translator = Translator
+	    .getTranslator(NewAnalysisStartPage.class);
+
     protected NewAnalysisStartPage() {
 	super("Source Directory");
-	setTitle("General Settings");
-	setMessage("Provide the general settings for the new analysis.");
+	setTitle(translator.i18n("General Settings"));
+	setMessage(translator
+		.i18n("Provide the general settings for the new analysis."));
     }
 
     @Override
