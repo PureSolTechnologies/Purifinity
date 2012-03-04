45bb7f3122c42780d10c41310708c478c433de1e
Rick-Rainer Ludwig <ludwig@puresol-technologies.com>
2012-03-04 21:35:39 +0100

    New sorting for package structures and trying to understand the functionality of tycho. The first bundle/plugin is implemented (uhura) and it is working. The next bundles are implemented next.

--- lib/FortranLanguagePack/src/test/resources/com/puresol/coding/lang/fortran/samples/EmptyProgram.f
+++
@@ -1,6 +0,0 @@
-      PROGRAM EmptyProgram
-C     This is a simple empty test program for testing the fortram
-C     analyser
-C
-      END PROGRAM
-C
--- lib/PureSolTechnologiesAPI/src/main/java/com/puresol/io/Encoding.java
+++
@@ -1,5 +0,0 @@
-package com.puresol.io;
-
-public enum Encoding {
-	BIG_ENDIAN, LITTLE_ENDIAN;
-}
