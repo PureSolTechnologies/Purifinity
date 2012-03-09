3829aaf920f06d61afa852b34b494628a32315d8
Rick-Rainer Ludwig <ludwig@puresol-technologies.com>
2012-03-09 20:04:46 +0100

    Got everything compiling and Eclipse IDE running. The Maven rcp process is still not working.

--- lib/com.puresol.trees/src/main/java/com/puresol/trees/Activator.java
+++ lib/com.puresol.trees/src/main/java/com/puresol/trees/Activator.java
@@ -2,23 +2,38 @@ package com.puresol.trees;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Activator implements BundleActivator {
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
-	 */
-	public void start(BundleContext context) throws Exception {
-		System.out.println("Hello World!!");
-	}
-	
-	/*
-	 * (non-Javadoc)
-	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
-	 */
-	public void stop(BundleContext context) throws Exception {
-		System.out.println("Goodbye World!!");
-	}
+    private static final Logger logger = LoggerFactory
+	    .getLogger(Activator.class);
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext
+     * )
+     */
+    @Override
+    public void start(BundleContext context) throws Exception {
+	logger.info("Staring bundle " + getClass().getPackage().getName()
+		+ "...");
+	System.out.println("Hello World!!");
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+     */
+    @Override
+    public void stop(BundleContext context) throws Exception {
+	logger.info("Stopping bundle " + getClass().getPackage().getName()
+		+ "...");
+    }
 
 }
