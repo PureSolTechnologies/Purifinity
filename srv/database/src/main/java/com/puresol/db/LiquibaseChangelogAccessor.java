3675a12022fdc3f842d92a66b2ba1e3f31c0e0db
Rick-Rainer Ludwig <ludwig@puresol-technologies.com>
2012-04-02 22:00:35 +0200

    Got liquibase working.

---
+++ srv/database/src/main/java/com/puresol/db/LiquibaseChangelogAccessor.java
@@ -0,0 +1,30 @@
+package com.puresol.db;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Enumeration;
+
+import liquibase.resource.ResourceAccessor;
+
+public class LiquibaseChangelogAccessor implements ResourceAccessor {
+
+    public static final String LIQUIBASE_CHANGELOG_RESOURCE = "/liquibase/changelog.xml";
+
+    @Override
+    public ClassLoader toClassLoader() {
+	return LiquibaseChangelogAccessor.class.getClassLoader();
+    }
+
+    @Override
+    public Enumeration<URL> getResources(String packageName) throws IOException {
+	return LiquibaseChangelogAccessor.class.getClassLoader().getResources(
+		packageName);
+    }
+
+    @Override
+    public InputStream getResourceAsStream(String file) throws IOException {
+	return LiquibaseChangelogAccessor.class.getResourceAsStream(file);
+    }
+
+}
--- srv/database/src/test/java/com/puresol/coding/db/DatabaseSchemaTest.java
+++ srv/database/src/test/java/com/puresol/coding/db/DatabaseSchemaTest.java
@@ -4,23 +4,62 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.postgresql.util.PSQLException;
+
+import com.puresol.db.LiquibaseHelper;
 
 public class DatabaseSchemaTest {
 
-    @Test
-    public void testAnalysisStoreToFileStoreFK() throws Exception {
+    @BeforeClass
+    public static void initialize() throws Exception {
 	Class.forName("org.postgresql.Driver");
-	Connection connection = DriverManager.getConnection(
+    }
+
+    private Connection connection;
+
+    @Before
+    public void setup() throws Exception {
+	LiquibaseHelper.dropAndUpdate();
+	connection = DriverManager.getConnection(
 		"jdbc:postgresql://localhost/CODEANALYSIS", "liquibase",
 		"liquibase");
+    }
+
+    @After
+    public void tearDown() throws Exception {
+	connection.close();
+    }
+
+    @Test(expected = PSQLException.class)
+    public void testAnalysisStoreToFileStoreFK() throws Exception {
 	try {
 	    Statement statement = connection.createStatement();
 	    try {
-		// statement
-		// .execute("INSERT INTO AnalysisStore (Key, Language, Time, RunTime, ParserTree) VALUES (1,'Java', '2012-01-02 12:00:01', 1234, 'ParserTree' )");
-		// TODO add analysis without file in file store and expect
-		// exception!
+		statement
+			.execute("INSERT INTO \"AnalysisStore\" (\"ParserTreeKey\") VALUES ('1234')");
+		connection.commit();
+	    } finally {
+		statement.close();
+	    }
+	} finally {
+	    connection.close();
+	}
+    }
+
+    @Test
+    public void testAnalysisStoreToFileStoreFK2() throws Exception {
+	try {
+	    Statement statement = connection.createStatement();
+	    try {
+		statement
+			.execute("INSERT INTO \"FileStore\" (\"FileKey\") VALUES ('123')");
+		statement
+			.execute("INSERT INTO \"AnalysisStore\" (\"ParserTreeKey\") VALUES ('123')");
+		connection.commit();
 	    } finally {
 		statement.close();
 	    }
