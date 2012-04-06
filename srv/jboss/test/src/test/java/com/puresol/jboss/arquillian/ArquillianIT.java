a775eaed48560c3cedc954202bf7e22829c4fe66
Rick-Rainer Ludwig <ludwig@puresol-technologies.com>
2012-04-06 13:19:48 +0200

    Some minor improvements.

--- srv/jboss/test/src/test/java/com/puresol/jboss/arquillian/ArquillianIT.java
+++ srv/jboss/test/src/test/java/com/puresol/jboss/arquillian/ArquillianIT.java
@@ -1,15 +1,7 @@
 package com.puresol.jboss.arquillian;
 
-import static org.junit.Assert.assertNotNull;
-
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URL;
-
 import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.junit.Arquillian;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -17,26 +9,9 @@ import org.junit.runner.RunWith;
 @RunAsClient
 public class ArquillianIT {
 
-    @BeforeClass
-    public static void initialize() throws Exception {
-	URL resource = ArquillianIT.class.getResource("/arquillian.xml");
-	assertNotNull(resource);
-	InputStream inStream = resource.openStream();
-	try {
-	    BufferedReader bufferedReader = new BufferedReader(
-		    new InputStreamReader(inStream));
-	    String line;
-	    while ((line = bufferedReader.readLine()) != null) {
-		System.out.println(line);
-	    }
-	} finally {
-	    inStream.close();
-	}
-    }
-
     @Test
     public void test() {
-
+	System.out.println("Test!");
     }
 
 }
