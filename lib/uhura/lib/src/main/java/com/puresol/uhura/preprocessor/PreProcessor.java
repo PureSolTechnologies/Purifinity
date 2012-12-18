f407a0929181f47ef900585d0ecc0bee94e71894
Rick-Rainer Ludwig <ludwig@puresol-technologies.com>
2012-12-18 21:43:28 +0100

    Renamed directories and added new project for CPP (C Preprocessor).

---
+++ lib/uhura/lib/src/main/java/com/puresol/uhura/preprocessor/PreProcessor.java
@@ -0,0 +1,21 @@
+package com.puresol.uhura.preprocessor;
+
+import com.puresol.uhura.lexer.SourceCode;
+
+/**
+ * This is an interface for PreProcessor implementations. A PreProcessor is
+ * meant to pre-process a source code to introduce more information or to
+ * extract information which are not part of the source itself like control and
+ * storage information.
+ * 
+ * A famous pre-processor is the C Preprocessor which is also implemented with
+ * this interface.
+ * 
+ * @author Rick-Rainer Ludwig
+ * 
+ */
+public interface PreProcessor {
+
+    public SourceCode process(SourceCode sourceCode);
+
+}
