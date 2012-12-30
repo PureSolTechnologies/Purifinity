99b4a28c38157e82e0b9474068e87e78d63862c7
Rick-Rainer Ludwig <ludwig@puresol-technologies.com>
2012-12-30 13:39:59 +0100

    Package refactoring.

---
+++ lib/coding/lang/cpp11/src/main/java/com/puresol/coding/lang/cpp11/IncludeDirectories.java
@@ -0,0 +1,42 @@
+package com.puresol.coding.lang.cpp11;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This class handles and contains all directories which are used for include
+ * macros.
+ * 
+ * @author Rick-Rainer Ludwig
+ */
+public class IncludeDirectories {
+
+    private static final Object INSTANCE_LOCK = new Object();
+    private static IncludeDirectories instance = null;
+
+    public static IncludeDirectories getInstance() {
+	if (instance == null) {
+	    synchronized (INSTANCE_LOCK) {
+		if (instance == null) {
+		    instance = new IncludeDirectories();
+		}
+	    }
+	}
+	return instance;
+    }
+
+    private final List<File> directories = new ArrayList<File>();
+
+    public List<File> getDirectories() {
+	return directories;
+    }
+
+    public void addDirectory(File directory) {
+	directories.add(directory);
+    }
+
+    public void removeDirectory(File directory) {
+	directories.remove(directory);
+    }
+}
