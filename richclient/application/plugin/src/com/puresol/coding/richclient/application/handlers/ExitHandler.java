8e915bf6cfea97f91b779d7d660c01de9616e1cd
Rick-Rainer Ludwig <ludwig@puresol-technologies.com>
2013-02-05 20:05:23 +0100

    Fixed build for .bundle projects. We go now over a lib directory.

--- richclient/application/plugin/src/com/puresol/coding/richclient/application/handlers/ExitHandler.java
+++ richclient/application/plugin/src/com/puresol/coding/richclient/application/handlers/ExitHandler.java
@@ -3,11 +3,21 @@ package com.puresol.coding.richclient.application.handlers;
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.ui.workbench.IWorkbench;
 
+import com.puresol.data.HashCodeGenerator;
+import com.puresol.trees.FileTree;
+import com.puresol.trees.Tree;
+import com.puresol.uhura.lexer.TokenMetaData;
+import com.puresol.uhura.source.FixedCodeLocation;
+import com.puresol.utils.HashAlgorithm;
+
 @SuppressWarnings("restriction")
 public class ExitHandler {
 
 	@Execute
 	public void execute(IWorkbench workbench) {
+		Tree<?> tree = new FileTree(null, "Name");
+		HashCodeGenerator.get(HashAlgorithm.SHA256, tree.toString());
+		new TokenMetaData(new FixedCodeLocation("H"), 1, 2).clone();
 		workbench.close();
 	}
 
--- richclient/application/plugin/src/com/puresol/coding/richclient/application/utils/TestTest.java
+++ richclient/application/plugin/src/com/puresol/coding/richclient/application/utils/TestTest.java
@@ -1,12 +1,8 @@
 package com.puresol.coding.richclient.application.utils;
 
-import com.puresol.trees.FileTree;
-import com.puresol.trees.Tree;
 
 public class TestTest {
 
 	public void test() {
-		Tree<?> tree = new FileTree(null, "parent");
-		tree.getName();
 	}
 }
