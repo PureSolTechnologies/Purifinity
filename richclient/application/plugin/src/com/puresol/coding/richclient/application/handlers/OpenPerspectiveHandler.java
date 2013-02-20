03e94d35f86d241b84f15a57c4702f03b1ab4db4
Rick-Rainer Ludwig <ludwig@puresol-technologies.com>
2013-02-20 16:59:04 +0100

    I did some work to get the compatibility layer working, but did not succeed. Check now to get Indigo working again.

--- richclient/application/plugin/src/com/puresol/coding/richclient/application/handlers/OpenPerspectiveHandler.java
+++ richclient/application/plugin/src/com/puresol/coding/richclient/application/handlers/OpenPerspectiveHandler.java
@@ -1,14 +1,32 @@
 package com.puresol.coding.richclient.application.handlers;
 
+import org.eclipse.core.commands.Command;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.ParameterizedCommand;
+import org.eclipse.e4.core.commands.ECommandService;
+import org.eclipse.e4.core.commands.EHandlerService;
 import org.eclipse.e4.core.di.annotations.Execute;
-import org.eclipse.e4.ui.model.application.MApplication;
-import org.eclipse.e4.ui.workbench.modeling.EPartService;
+import org.eclipse.ui.handlers.ShowPerspectiveHandler;
 
 @SuppressWarnings("restriction")
 public class OpenPerspectiveHandler {
 
 	@Execute
-	public void execute(MApplication application, EPartService partService) {
+	public void execute(ECommandService commandService,
+			EHandlerService handlerService) throws ExecutionException {
+		ShowPerspectiveHandler handler = new ShowPerspectiveHandler();
+		handlerService.activateHandler(
+				"org.eclipse.ui.perspectives.showPerspective", handler);
+		Command command = commandService
+				.getCommand("org.eclipse.ui.perspectives.showPerspective");
+		ParameterizedCommand createdCommand;
+		if (command == null) {
+			createdCommand = commandService.createCommand(
+					"org.eclipse.ui.perspectives.showPerspective", null);
+		} else {
+			createdCommand = new ParameterizedCommand(command, null);
+		}
+		handlerService.executeHandler(createdCommand, null);
 	}
 
 }
---
+++ richclient/features/contrib/plugin/src/com/puresol/coding/richclient/features/contrib/Activate.java
@@ -0,0 +1,20 @@
+package com.puresol.coding.richclient.features.contrib;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activate implements BundleActivator {
+
+	@Override
+	public void start(BundleContext context) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("Test");
+	}
+
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
