e0d5350ab92035f3c04b4374c48eb84cd9ae577b
Rick-Rainer Ludwig <ludwig@puresol-technologies.com>
2013-02-22 23:55:09 +0100

    Solved issue of starting, compiling and developing RCP client. We use Eclipse 3.x (3.7) style for development and can use Juno or Keplar for development IDE:

--- base/trees/lib/src/main/java/com/puresol/trees/AbstractTreeImpl.java
+++ base/trees/lib/src/main/java/com/puresol/trees/AbstractTreeImpl.java
@@ -13,59 +13,59 @@ import java.util.List;
  *            is the type of the nodes. T needs to implement AbstractTreeImpl.
  */
 public class AbstractTreeImpl<T extends AbstractTreeImpl<T>> implements
-	Tree<T>, Iterable<T> {
+		Tree<T>, Iterable<T> {
 
-    private final T parent;
-    private final List<T> children = new ArrayList<T>();
-    private final String name;
+	private final T parent;
+	private final List<T> children = new ArrayList<T>();
+	private final String name;
 
-    /**
-     * This is the initial value constructor.
-     * 
-     * @param parent
-     *            is the parent where this node is to be added to.
-     * @param name
-     *            is the identifier of this node.
-     */
-    public AbstractTreeImpl(T parent, String name) {
-	super();
-	this.parent = parent;
-	if (parent != null) {
-	    @SuppressWarnings("unchecked")
-	    T t = (T) this;
-	    parent.addChild(t);
+	/**
+	 * This is the initial value constructor.
+	 * 
+	 * @param parent
+	 *            is the parent where this node is to be added to.
+	 * @param name
+	 *            is the identifier of this node.
+	 */
+	public AbstractTreeImpl(T parent, String name) {
+		super();
+		this.parent = parent;
+		if (parent != null) {
+			@SuppressWarnings("unchecked")
+			T t = (T) this;
+			parent.addChild(t);
+		}
+		this.name = name;
 	}
-	this.name = name;
-    }
 
-    private void addChild(T child) {
-	children.add(child);
-    }
+	protected void addChild(T child) {
+		children.add(child);
+	}
 
-    @Override
-    public T getParent() {
-	return parent;
-    }
+	@Override
+	public T getParent() {
+		return parent;
+	}
 
-    @Override
-    public boolean hasChildren() {
-	return children.size() > 0;
-    }
+	@Override
+	public boolean hasChildren() {
+		return children.size() > 0;
+	}
 
-    @Override
-    public List<T> getChildren() {
-	return children;
-    }
+	@Override
+	public List<T> getChildren() {
+		return children;
+	}
 
-    @Override
-    public String getName() {
-	return name;
-    }
+	@Override
+	public String getName() {
+		return name;
+	}
 
-    @Override
-    public Iterator<T> iterator() {
-	@SuppressWarnings("unchecked")
-	T t = (T) this;
-	return new TreeIterator<T>(t);
-    }
+	@Override
+	public Iterator<T> iterator() {
+		@SuppressWarnings("unchecked")
+		T t = (T) this;
+		return new TreeIterator<T>(t);
+	}
 }
