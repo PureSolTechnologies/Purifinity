5fd0bd2f6a3e7eea8ca78300cb9d1b34cf312556
Rick-Rainer Ludwig <ludwig@puresol-technologies.com>
2013-02-23 14:48:48 +0100

    Finished DirectoryRepositoryLocation.

--- base/coding/analysis/api/lib/src/main/java/com/puresol/coding/analysis/api/DirectoryRepositoryLocation.java
+++ base/coding/analysis/api/lib/src/main/java/com/puresol/coding/analysis/api/DirectoryRepositoryLocation.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import com.puresol.trees.FileTree;
 import com.puresol.uhura.source.CodeLocation;
+import com.puresol.uhura.source.SourceFileLocation;
 import com.puresol.utils.FileSearch;
 
 /**
@@ -15,49 +16,52 @@ import com.puresol.utils.FileSearch;
  */
 public class DirectoryRepositoryLocation extends AbstractRepositoryLocation {
 
-	private static final long serialVersionUID = -5405680480509263585L;
+    private static final long serialVersionUID = -5405680480509263585L;
 
-	private final File repositoryDirectory;
+    private final File repositoryDirectory;
 
-	public DirectoryRepositoryLocation(String name, File repositoryDirectory) {
-		super(name);
-		this.repositoryDirectory = repositoryDirectory;
+    public DirectoryRepositoryLocation(String name, File repositoryDirectory) {
+	super(name);
+	this.repositoryDirectory = repositoryDirectory;
+    }
+
+    @Override
+    public List<CodeLocation> getSourceCodes() {
+	FileTree fileTree = FileSearch.getFileTree(repositoryDirectory,
+		getCodeSearchConfiguration());
+	List<CodeLocation> locations = new ArrayList<CodeLocation>();
+	for (FileTree file : fileTree) {
+	    locations.add(new SourceFileLocation(file.getPathFile(true)));
 	}
+	return locations;
+    }
 
-	@Override
-	public List<CodeLocation> getSourceCodes() {
-		FileTree fileTree = FileSearch.getFileTree(repositoryDirectory,
-				getCodeSearchConfiguration());
-		List<CodeLocation> locations = new ArrayList<CodeLocation>();
-		return locations;
-	}
+    @Override
+    public int hashCode() {
+	final int prime = 31;
+	int result = super.hashCode();
+	result = prime
+		* result
+		+ ((repositoryDirectory == null) ? 0 : repositoryDirectory
+			.hashCode());
+	return result;
+    }
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = super.hashCode();
-		result = prime
-				* result
-				+ ((repositoryDirectory == null) ? 0 : repositoryDirectory
-						.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (!super.equals(obj))
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		DirectoryRepositoryLocation other = (DirectoryRepositoryLocation) obj;
-		if (repositoryDirectory == null) {
-			if (other.repositoryDirectory != null)
-				return false;
-		} else if (!repositoryDirectory.equals(other.repositoryDirectory))
-			return false;
-		return true;
-	}
+    @Override
+    public boolean equals(Object obj) {
+	if (this == obj)
+	    return true;
+	if (!super.equals(obj))
+	    return false;
+	if (getClass() != obj.getClass())
+	    return false;
+	DirectoryRepositoryLocation other = (DirectoryRepositoryLocation) obj;
+	if (repositoryDirectory == null) {
+	    if (other.repositoryDirectory != null)
+		return false;
+	} else if (!repositoryDirectory.equals(other.repositoryDirectory))
+	    return false;
+	return true;
+    }
 
 }
