d8f8743494c040a171f20bd8673f2490e909416f
Rick-Rainer Ludwig <ludwig@puresol-technologies.com>
2013-02-23 13:56:29 +0100

    Some work on UI for showing available language and evaluator services.

---
+++ rcp/application/plugin/src/com/puresol/coding/client/application/content/AvailableEvaluatorsTableViewer.java
@@ -0,0 +1,78 @@
+package com.puresol.coding.client.application.content;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.eclipse.jface.viewers.ColumnLabelProvider;
+import org.eclipse.jface.viewers.IStructuredContentProvider;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.jface.viewers.TableViewerColumn;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Table;
+
+import com.puresol.coding.evaluation.api.EvaluatorFactory;
+
+public class AvailableEvaluatorsTableViewer extends TableViewer implements
+	IStructuredContentProvider {
+
+    private final List<EvaluatorFactory> evaluators = new ArrayList<EvaluatorFactory>();
+
+    public AvailableEvaluatorsTableViewer(Table table) {
+	super(table);
+	setContentProvider(this);
+	setupNameColumn();
+	setupDescriptionColumn();
+    }
+
+    private void setupNameColumn() {
+	TableViewerColumn nameColumn = new TableViewerColumn(this, SWT.NONE);
+	nameColumn.getColumn().setText("Name");
+	nameColumn.getColumn().setWidth(100);
+	nameColumn.setLabelProvider(new ColumnLabelProvider() {
+	    @Override
+	    public String getText(Object element) {
+		EvaluatorFactory evaluator = (EvaluatorFactory) element;
+		return evaluator.getName();
+	    }
+	});
+    }
+
+    private void setupDescriptionColumn() {
+	TableViewerColumn nameColumn = new TableViewerColumn(this, SWT.NONE);
+	nameColumn.getColumn().setText("Description");
+	nameColumn.getColumn().setWidth(100);
+	nameColumn.setLabelProvider(new ColumnLabelProvider() {
+	    @Override
+	    public String getText(Object element) {
+		EvaluatorFactory evaluator = (EvaluatorFactory) element;
+		return evaluator.getDescription();
+	    }
+	});
+    }
+
+    @Override
+    public void dispose() {
+    }
+
+    @Override
+    public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
+	evaluators.clear();
+	if (newInput == null) {
+	    return;
+	}
+	if (Collection.class.isAssignableFrom(newInput.getClass())) {
+	    @SuppressWarnings("unchecked")
+	    Collection<EvaluatorFactory> collection = (Collection<EvaluatorFactory>) newInput;
+	    evaluators.addAll(collection);
+	}
+	refresh();
+    }
+
+    @Override
+    public EvaluatorFactory[] getElements(Object inputElement) {
+	return evaluators.toArray(new EvaluatorFactory[evaluators.size()]);
+    }
+
+}
---
+++ rcp/application/plugin/src/com/puresol/coding/client/application/content/AvailableLanguagesTableViewer.java
@@ -0,0 +1,79 @@
+package com.puresol.coding.client.application.content;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.eclipse.jface.viewers.ColumnLabelProvider;
+import org.eclipse.jface.viewers.IStructuredContentProvider;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.jface.viewers.TableViewerColumn;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Table;
+
+import com.puresol.coding.analysis.api.AnalyzableProgrammingLanguage;
+
+public class AvailableLanguagesTableViewer extends TableViewer implements
+	IStructuredContentProvider {
+
+    private final List<AnalyzableProgrammingLanguage> languages = new ArrayList<AnalyzableProgrammingLanguage>();
+
+    public AvailableLanguagesTableViewer(Table table) {
+	super(table);
+	setContentProvider(this);
+	setupNameColumn();
+	setupVersionColumn();
+    }
+
+    private void setupNameColumn() {
+	TableViewerColumn nameColumn = new TableViewerColumn(this, SWT.NONE);
+	nameColumn.getColumn().setText("Name");
+	nameColumn.getColumn().setWidth(100);
+	nameColumn.setLabelProvider(new ColumnLabelProvider() {
+	    @Override
+	    public String getText(Object element) {
+		AnalyzableProgrammingLanguage language = (AnalyzableProgrammingLanguage) element;
+		return language.getName();
+	    }
+	});
+    }
+
+    private void setupVersionColumn() {
+	TableViewerColumn nameColumn = new TableViewerColumn(this, SWT.NONE);
+	nameColumn.getColumn().setText("Version");
+	nameColumn.getColumn().setWidth(100);
+	nameColumn.setLabelProvider(new ColumnLabelProvider() {
+	    @Override
+	    public String getText(Object element) {
+		AnalyzableProgrammingLanguage language = (AnalyzableProgrammingLanguage) element;
+		return language.getVersion();
+	    }
+	});
+    }
+
+    @Override
+    public void dispose() {
+    }
+
+    @Override
+    public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
+	languages.clear();
+	if (newInput == null) {
+	    return;
+	}
+	if (Collection.class.isAssignableFrom(newInput.getClass())) {
+	    @SuppressWarnings("unchecked")
+	    Collection<AnalyzableProgrammingLanguage> collection = (Collection<AnalyzableProgrammingLanguage>) newInput;
+	    languages.addAll(collection);
+	}
+	refresh();
+    }
+
+    @Override
+    public AnalyzableProgrammingLanguage[] getElements(Object inputElement) {
+	return languages.toArray(new AnalyzableProgrammingLanguage[languages
+		.size()]);
+    }
+
+}
