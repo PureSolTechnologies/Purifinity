07afe6ccb7df9944ef10d5bfcdc47f9103acd4ab
Rick-Rainer Ludwig <ludwig@puresol-technologies.com>
2013-02-27 20:03:51 +0100

    Some UI stuff.

---
+++ rcp/application/plugin/src/com/puresol/coding/client/application/PurifinityIntroPart.java
@@ -0,0 +1,33 @@
+package com.puresol.coding.client.application;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.ui.part.IntroPart;
+
+public class PurifinityIntroPart extends IntroPart {
+    private Text text;
+
+    @Override
+    public void standbyStateChanged(boolean standby) {
+    }
+
+    /**
+     * @wbp.parser.entryPoint
+     */
+    @Override
+    public void createPartControl(Composite parent) {
+	Composite composite = new Composite(parent, SWT.NONE);
+	composite.setLayout(new FillLayout(SWT.HORIZONTAL));
+
+	text = new Text(composite, SWT.BORDER);
+	text.setEditable(false);
+	text.setText("Welcome to Purifinity!\n\nIt is the tool No. 1 for source code analysis.");
+    }
+
+    @Override
+    public void setFocus() {
+	text.setFocus();
+    }
+}
