c7bd67a447f69b449b7f737af00800fbd5e7f8c9
Rick-Rainer Ludwig <ludwig@puresol-technologies.com>
2013-04-07 19:17:16 +0200

    Refactoring.

--- rcp/common/evaluation.plugin/src/com/puresol/coding/client/common/evaluation/views/MetricsMapView.java
+++ rcp/common/evaluation.plugin/src/com/puresol/coding/client/common/evaluation/views/MetricsMapView.java
@@ -47,315 +47,346 @@ import com.puresol.utils.math.Parameter;
 import com.puresol.utils.math.Value;
 
 public class MetricsMapView extends ViewPart implements Refreshable,
-		Reproducable, ISelectionListener, EvaluationsTarget,
-		PartSettingsCapability {
+	Reproducable, ISelectionListener, EvaluationsTarget,
+	PartSettingsCapability {
 
-	private ISelectionService selectionService;
-	private FileAnalysisSelection analysisSelection;
+    private ISelectionService selectionService;
+    private FileAnalysisSelection analysisSelection;
 
-	private Label label;
+    private Label label;
 
-	private Composite container;
-	private AreaMapComponent areaMap;
+    private Composite container;
+    private AreaMapComponent areaMap;
 
-	private MetricsMapViewSettingsDialog settingsDialog;
+    private MetricsMapViewSettingsDialog settingsDialog;
 
-	private EvaluatorFactory mapMetricSelection = null;
-	private Parameter<?> mapValueSelection = null;
-	private EvaluatorFactory colorMetricSelection = null;
-	private Parameter<?> colorValueSelection = null;
+    private EvaluatorFactory mapMetricSelection = null;
+    private Parameter<?> mapValueSelection = null;
+    private EvaluatorFactory colorMetricSelection = null;
+    private Parameter<?> colorValueSelection = null;
 
-	public MetricsMapView() {
-		super();
+    public MetricsMapView() {
+	super();
+    }
+
+    @Override
+    public void dispose() {
+	selectionService.removeSelectionListener(this);
+	super.dispose();
+    }
+
+    /**
+     * Create contents of the view part.
+     * 
+     * @param parent
+     */
+    @Override
+    public void createPartControl(Composite parent) {
+	container = new Composite(parent, SWT.BORDER);
+	container.setLayout(new FormLayout());
+
+	label = new Label(container, SWT.BORDER);
+	{
+	    FormData fd_label = new FormData();
+	    fd_label.left = new FormAttachment(0, 10);
+	    fd_label.right = new FormAttachment(100, -10);
+	    fd_label.top = new FormAttachment(0, 10);
+	    label.setLayoutData(fd_label);
 	}
 
-	@Override
-	public void dispose() {
-		selectionService.removeSelectionListener(this);
-		super.dispose();
+	areaMap = new AreaMapComponent(container, SWT.NONE);
+	{
+	    FormData fd_areaMap = new FormData();
+	    fd_areaMap.left = new FormAttachment(label, 0, SWT.LEFT);
+	    fd_areaMap.right = new FormAttachment(label, 0, SWT.RIGHT);
+	    fd_areaMap.top = new FormAttachment(label, 10);
+	    fd_areaMap.bottom = new FormAttachment(100, -10);
+	    areaMap.setLayoutData(fd_areaMap);
 	}
 
-	/**
-	 * Create contents of the view part.
-	 * 
-	 * @param parent
-	 */
-	@Override
-	public void createPartControl(Composite parent) {
-		container = new Composite(parent, SWT.BORDER);
-		container.setLayout(new FormLayout());
+	IWorkbenchPartSite site = getSite();
+	IWorkbenchWindow workbenchWindow = site.getWorkbenchWindow();
+	selectionService = workbenchWindow.getSelectionService();
+	selectionService.addSelectionListener(this);
 
-		label = new Label(container, SWT.BORDER);
-		{
-			FormData fd_label = new FormData();
-			fd_label.left = new FormAttachment(0, 10);
-			fd_label.right = new FormAttachment(100, -10);
-			fd_label.top = new FormAttachment(0, 10);
-			label.setLayoutData(fd_label);
-		}
+	initializeToolBar();
+	initializeMenu();
+    }
 
-		areaMap = new AreaMapComponent(container, SWT.NONE);
-		{
-			FormData fd_areaMap = new FormData();
-			fd_areaMap.left = new FormAttachment(label, 0, SWT.LEFT);
-			fd_areaMap.right = new FormAttachment(label, 0, SWT.RIGHT);
-			fd_areaMap.top = new FormAttachment(label, 10);
-			fd_areaMap.bottom = new FormAttachment(100, -10);
-			areaMap.setLayoutData(fd_areaMap);
-		}
+    /**
+     * Initialize the toolbar.
+     */
+    private void initializeToolBar() {
+	IToolBarManager toolbarManager = getViewSite().getActionBars()
+		.getToolBarManager();
+	toolbarManager.add(new ShowSettingsAction(this));
+	toolbarManager.add(new ViewReproductionAction(this));
+	toolbarManager.add(new RefreshAction(this));
+    }
 
-		IWorkbenchPartSite site = getSite();
-		IWorkbenchWindow workbenchWindow = site.getWorkbenchWindow();
-		selectionService = workbenchWindow.getSelectionService();
-		selectionService.addSelectionListener(this);
+    /**
+     * Initialize the menu.
+     */
+    private void initializeMenu() {
+	IMenuManager menuManager = getViewSite().getActionBars()
+		.getMenuManager();
+	menuManager.add(new RefreshAction(this));
+    }
 
-		initializeToolBar();
-		initializeMenu();
+    @Override
+    public void init(IViewSite site, IMemento memento) throws PartInitException {
+	super.init(site, memento);
+	if (memento == null) {
+	    return;
+	}
+	// touch old classes to get the plugins activated... :-(
+	String mapMeticClass = memento.getString("map.metric.class");
+	if (mapMeticClass != null) {
+	    try {
+		Class.forName(mapMeticClass);
+	    } catch (ClassNotFoundException e) {
+	    }
+	}
+	String colorMeticClass = memento.getString("color.metric.class");
+	if (colorMeticClass != null) {
+	    try {
+		Class.forName(colorMeticClass);
+	    } catch (ClassNotFoundException e) {
+	    }
 	}
 
-	/**
-	 * Initialize the toolbar.
-	 */
-	private void initializeToolBar() {
-		IToolBarManager toolbarManager = getViewSite().getActionBars()
-				.getToolBarManager();
-		toolbarManager.add(new ShowSettingsAction(this));
-		toolbarManager.add(new ViewReproductionAction(this));
-		toolbarManager.add(new RefreshAction(this));
-	}
-
-	/**
-	 * Initialize the menu.
-	 */
-	private void initializeMenu() {
-		IMenuManager menuManager = getViewSite().getActionBars()
-				.getMenuManager();
-		menuManager.add(new RefreshAction(this));
-	}
-
-	@Override
-	public void init(IViewSite site, IMemento memento) throws PartInitException {
-		super.init(site, memento);
-		if (memento == null) {
-			return;
-		}
-		// touch old classes to get the plugins activated... :-(
-		String mapMeticClass = memento.getString("map.metric.class");
-		if (mapMeticClass != null) {
-			try {
-				Class.forName(mapMeticClass);
-			} catch (ClassNotFoundException e) {
+	List<EvaluatorFactory> allMetrics = Evaluators.createInstance()
+		.getAllMetrics();
+	String mapMetricSelectionName = memento.getString("map.metric");
+	String mapValueSelectionName = memento.getString("map.value");
+	for (EvaluatorFactory metric : allMetrics) {
+	    if (metric.getName().equals(mapMetricSelectionName)) {
+		mapMetricSelection = metric;
+		if (mapValueSelectionName != null) {
+		    for (Parameter<?> parameter : mapMetricSelection
+			    .getParameters()) {
+			if (parameter.getName().equals(mapValueSelectionName)) {
+			    mapValueSelection = parameter;
+			    break;
 			}
+		    }
 		}
-		String colorMeticClass = memento.getString("color.metric.class");
-		if (colorMeticClass != null) {
-			try {
-				Class.forName(colorMeticClass);
-			} catch (ClassNotFoundException e) {
+		break;
+	    }
+	}
+	String colorMetricSelectionName = memento.getString("color.metric");
+	String colorValueSelectionName = memento.getString("color.value");
+	for (EvaluatorFactory metric : allMetrics) {
+	    if (metric.getName().equals(colorMetricSelectionName)) {
+		colorMetricSelection = metric;
+		if (colorValueSelectionName != null) {
+		    for (Parameter<?> parameter : colorMetricSelection
+			    .getParameters()) {
+			if (parameter.getName().equals(colorValueSelectionName)) {
+			    colorValueSelection = parameter;
+			    break;
 			}
+		    }
 		}
-
-		List<EvaluatorFactory> allMetrics = Evaluators.createInstance()
-				.getAllMetrics();
-		String mapMetricSelectionName = memento.getString("map.metric");
-		String mapValueSelectionName = memento.getString("map.value");
-		for (EvaluatorFactory metric : allMetrics) {
-			if (metric.getName().equals(mapMetricSelectionName)) {
-				mapMetricSelection = metric;
-				if (mapValueSelectionName != null) {
-					for (Parameter<?> parameter : mapMetricSelection
-							.getParameters()) {
-						if (parameter.getName().equals(mapValueSelectionName)) {
-							mapValueSelection = parameter;
-							break;
-						}
-					}
-				}
-				break;
-			}
-		}
-		String colorMetricSelectionName = memento.getString("color.metric");
-		String colorValueSelectionName = memento.getString("color.value");
-		for (EvaluatorFactory metric : allMetrics) {
-			if (metric.getName().equals(colorMetricSelectionName)) {
-				colorMetricSelection = metric;
-				if (colorValueSelectionName != null) {
-					for (Parameter<?> parameter : colorMetricSelection
-							.getParameters()) {
-						if (parameter.getName().equals(colorValueSelectionName)) {
-							colorValueSelection = parameter;
-							break;
-						}
-					}
-				}
-				break;
-			}
-		}
+		break;
+	    }
 	}
+    }
 
-	@Override
-	public void saveState(IMemento memento) {
-		memento.putString("map.metric.class", mapMetricSelection.getClass()
-				.getName());
-		memento.putString("map.metric", mapMetricSelection.getName());
-		memento.putString("map.value", mapValueSelection.getName());
-		memento.putString("color.metric.class", colorMetricSelection.getClass()
-				.getName());
-		memento.putString("color.metric", colorMetricSelection.getName());
-		memento.putString("color.value", colorValueSelection.getName());
+    @Override
+    public void saveState(IMemento memento) {
+	memento.putString("map.metric.class", mapMetricSelection.getClass()
+		.getName());
+	memento.putString("map.metric", mapMetricSelection.getName());
+	memento.putString("map.value", mapValueSelection.getName());
+	memento.putString("color.metric.class", colorMetricSelection.getClass()
+		.getName());
+	memento.putString("color.metric", colorMetricSelection.getName());
+	memento.putString("color.value", colorValueSelection.getName());
 
-		super.saveState(memento);
+	super.saveState(memento);
+    }
+
+    @Override
+    public void setFocus() {
+	areaMap.setFocus();
+    }
+
+    @Override
+    public void refresh() {
+	if (settingsDialog != null) {
+	    settingsDialog.refresh();
 	}
+    }
 
-	@Override
-	public void setFocus() {
-		areaMap.setFocus();
+    @Override
+    public void selectionChanged(IWorkbenchPart part, ISelection selection) {
+	if (selection instanceof FileAnalysisSelection) {
+	    analysisSelection = (FileAnalysisSelection) selection;
+	    updateEvaluation();
 	}
+    }
 
-	@Override
-	public void refresh() {
-		if (settingsDialog != null) {
-			settingsDialog.refresh();
+    private void updateEvaluation() {
+	if ((analysisSelection != null) && (mapMetricSelection != null)
+		&& (mapValueSelection != null)) {
+	    HashIdFileTree path = analysisSelection.getHashIdFile();
+	    if (path.isFile()) {
+		path = path.getParent();
+	    }
+	    showEvaluation(path);
+	}
+    }
+
+    @Override
+    public void showEvaluation(HashIdFileTree path) {
+	label.setText(path.getPathFile(false).getPath());
+	EvaluatorStore mapStore = EvaluatorStoreFactory.getFactory()
+		.createInstance(mapMetricSelection.getEvaluatorClass());
+	AreaMapData data = calculateAreaData(mapStore, path);
+	areaMap.setData(data, mapValueSelection.getUnit());
+    }
+
+    /**
+     * This method calculates the map data for the {@link AreaMapComponent}.
+     * 
+     * This method is called recursively.
+     * 
+     * @param mapStore
+     *            is the {@link EvaluatorStore} to be used to load metric
+     *            results.
+     * @param path
+     *            is the path to be calculated.
+     * @return
+     */
+    private AreaMapData calculateAreaData(EvaluatorStore mapStore,
+	    HashIdFileTree path) {
+	List<AreaMapData> childAreas = calculateChildAreaMaps(mapStore, path);
+	MetricResults results;
+	if (path.isFile()) {
+	    results = mapStore.readFileResults(path.getHashId());
+	} else {
+	    results = mapStore.readDirectoryResults(path.getHashId());
+	}
+	if ((results == null) || (results.getValues().size() == 0)) {
+	    return processAreaWithoutOwnValues(path,
+		    childAreas.toArray(new AreaMapData[childAreas.size()]));
+	}
+	double sum = findSuitableValue(path, results);
+	return new AreaMapData(path.getPathFile(false).toString(), sum,
+		childAreas.toArray(new AreaMapData[childAreas.size()]));
+    }
+
+    private double findSuitableValue(HashIdFileTree path, MetricResults results) {
+	Map<String, Value<?>> valueMap = null;
+	List<Map<String, Value<?>>> values = results.getValues();
+	if (path.isFile()) {
+	    if (values.size() == 1) {
+		valueMap = values.get(0);
+	    } else {
+		String codeRangeTypeParameterName = CodeRangeTypeParameter
+			.getInstance().getName();
+		for (Map<String, Value<?>> value : values) {
+		    if (value.get(codeRangeTypeParameterName).getValue()
+			    .equals(CodeRangeType.FILE)) {
+			valueMap = value;
+			break;
+		    }
 		}
+		if (valueMap == null) {
+		    throw new RuntimeException("File '"
+			    + path.getPathFile(false)
+			    + "' contains no FILE result for evaluator '"
+			    + mapMetricSelection.getName()
+			    + "' and contains multiple values!");
+		}
+	    }
+	} else {
+	    if (values.size() != 1) {
+		throw new RuntimeException("Directory '"
+			+ path.getPathFile(false)
+			+ "' contains more than one result for evaluator '"
+			+ mapMetricSelection.getName() + "'!");
+	    }
+	    valueMap = values.get(0);
 	}
+	return convertToDouble(path, valueMap);
+    }
 
-	@Override
-	public void selectionChanged(IWorkbenchPart part, ISelection selection) {
-		if (selection instanceof FileAnalysisSelection) {
-			analysisSelection = (FileAnalysisSelection) selection;
-			updateEvaluation();
-		}
+    private double convertToDouble(HashIdFileTree path,
+	    Map<String, Value<?>> valueMap) {
+	double sum = 0.0;
+	Value<?> value = valueMap.get(mapValueSelection.getName());
+	if (Number.class.isAssignableFrom(mapValueSelection.getType())) {
+	    sum = ((Number) value.getValue()).doubleValue();
+	} else {
+	    throw new RuntimeException("Value '" + value
+		    + "' is not a number for '" + path.getPathFile(false)
+		    + "'!");
 	}
+	return sum;
+    }
 
-	private void updateEvaluation() {
-		if ((analysisSelection != null) && (mapMetricSelection != null)
-				&& (mapValueSelection != null)) {
-			HashIdFileTree path = analysisSelection.getHashIdFile();
-			if (path.isFile()) {
-				path = path.getParent();
-			}
-			showEvaluation(path);
-		}
+    /**
+     * This method calculates the values for all {@link HashIdFileTree}'s
+     * children.
+     * 
+     * @param store
+     * @param path
+     * @return
+     */
+    private List<AreaMapData> calculateChildAreaMaps(EvaluatorStore store,
+	    HashIdFileTree path) {
+	List<HashIdFileTree> children = path.getChildren();
+	List<AreaMapData> childAreas = new ArrayList<AreaMapData>();
+	for (int i = 0; i < children.size(); i++) {
+	    AreaMapData areaData = calculateAreaData(store, children.get(i));
+	    if (areaData != null) {
+		childAreas.add(areaData);
+	    }
 	}
+	Collections.sort(childAreas);
+	return childAreas;
+    }
 
-	@Override
-	public void showEvaluation(HashIdFileTree path) {
-		label.setText(path.getPathFile(false).getPath());
-		EvaluatorStore mapStore = EvaluatorStoreFactory.getFactory()
-				.createInstance(mapMetricSelection.getEvaluatorClass());
-		AreaMapData data = calculateAreaData(mapStore, path);
-		areaMap.setData(data, mapValueSelection.getUnit());
+    private AreaMapData processAreaWithoutOwnValues(HashIdFileTree path,
+	    AreaMapData[] childAreas) {
+	double sum = 0.0;
+	for (AreaMapData childArea : childAreas) {
+	    sum += childArea.getValue();
 	}
+	if (sum > 0.0) {
+	    return new AreaMapData(path.getPathFile(false).toString(), sum,
+		    childAreas);
+	} else {
+	    return null;
+	}
+    }
 
-	private AreaMapData calculateAreaData(EvaluatorStore mapStore, HashIdFileTree path) {
-		List<AreaMapData> childAreas = calculateChildAreaMaps(mapStore, path);
-		MetricResults results;
-		if (path.isFile()) {
-			results = mapStore.readFileResults(path.getHashId());
-		} else {
-			results = mapStore.readDirectoryResults(path.getHashId());
-		}
-		if ((results == null) || (results.getValues().size() == 0)) {
-			return processAreaWithoutOwnValues(path,
-					childAreas.toArray(new AreaMapData[childAreas.size()]));
-		}
-		List<Map<String, Value<?>>> values = results.getValues();
-		Map<String, Value<?>> valueMap = null;
-		if (path.isFile()) {
-			String codeRangeTypeParameterName = CodeRangeTypeParameter
-					.getInstance().getName();
-			if (values.size() == 1) {
-				valueMap = values.get(0);
-			} else {
-				for (Map<String, Value<?>> value : values) {
-					if (value.get(codeRangeTypeParameterName).getValue()
-							.equals(CodeRangeType.FILE)) {
-						valueMap = value;
-						break;
-					}
-				}
-				if (valueMap == null) {
-					throw new RuntimeException("File '"
-							+ path.getPathFile(false)
-							+ "' contains no FILE result for evaluator '"
-							+ mapMetricSelection.getName()
-							+ "' and contains multiple values!");
-				}
-			}
-		} else {
-			if (values.size() != 1) {
-				throw new RuntimeException("Directory '"
-						+ path.getPathFile(false)
-						+ "' contains more than one result for evaluator '"
-						+ mapMetricSelection.getName() + "'!");
-			}
-			valueMap = values.get(0);
-		}
-		double sum = 0.0;
-		Value<?> value = valueMap.get(mapValueSelection.getName());
-		if (Number.class.isAssignableFrom(mapValueSelection.getType())) {
-			sum = ((Number) value.getValue()).doubleValue();
-		} else {
-			throw new RuntimeException("Value '" + value
-					+ "' is not a number for '" + path.getPathFile(false)
-					+ "'!");
-		}
-		return new AreaMapData(path.getPathFile(false).toString(), sum,
-				childAreas.toArray(new AreaMapData[childAreas.size()]));
+    @Override
+    public void showSettings() {
+	if (settingsDialog == null) {
+	    settingsDialog = new MetricsMapViewSettingsDialog(this,
+		    mapMetricSelection, mapValueSelection,
+		    colorMetricSelection, colorValueSelection);
+	    settingsDialog.open();
+	} else {
+	    settingsDialog.close();
+	    settingsDialog = null;
 	}
+    }
 
-	private List<AreaMapData> calculateChildAreaMaps(EvaluatorStore store,
-			HashIdFileTree path) {
-		List<HashIdFileTree> children = path.getChildren();
-		List<AreaMapData> childAreas = new ArrayList<AreaMapData>();
-		for (int i = 0; i < children.size(); i++) {
-			AreaMapData areaData = calculateAreaData(store, children.get(i));
-			if (areaData != null) {
-				childAreas.add(areaData);
-			}
-		}
-		Collections.sort(childAreas);
-		return childAreas;
-	}
+    @Override
+    public void closeSettings() {
+	settingsDialog = null;
+    }
 
-	private AreaMapData processAreaWithoutOwnValues(HashIdFileTree path,
-			AreaMapData[] childAreas) {
-		double sum = 0.0;
-		for (AreaMapData childArea : childAreas) {
-			sum += childArea.getValue();
-		}
-		if (sum > 0.0) {
-			return new AreaMapData(path.getPathFile(false).toString(), sum,
-					childAreas);
-		} else {
-			return null;
-		}
-	}
-
-	@Override
-	public void showSettings() {
-		if (settingsDialog == null) {
-			settingsDialog = new MetricsMapViewSettingsDialog(this,
-					mapMetricSelection, mapValueSelection,
-					colorMetricSelection, colorValueSelection);
-			settingsDialog.open();
-		} else {
-			settingsDialog.close();
-			settingsDialog = null;
-		}
-	}
-
-	@Override
-	public void closeSettings() {
-		settingsDialog = null;
-	}
-
-	@Override
-	public void applySettings() {
-		mapMetricSelection = settingsDialog.getMapMetric();
-		mapValueSelection = settingsDialog.getMapValue();
-		colorMetricSelection = settingsDialog.getColorMetric();
-		colorValueSelection = settingsDialog.getColorValue();
-		updateEvaluation();
-	}
+    @Override
+    public void applySettings() {
+	mapMetricSelection = settingsDialog.getMapMetric();
+	mapValueSelection = settingsDialog.getMapValue();
+	colorMetricSelection = settingsDialog.getColorMetric();
+	colorValueSelection = settingsDialog.getColorValue();
+	updateEvaluation();
+    }
 }
