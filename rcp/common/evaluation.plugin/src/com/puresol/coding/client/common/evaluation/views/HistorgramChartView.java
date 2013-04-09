d439a7a6c6419d3500ae7dd59e38d5293d0d671f
Rick-Rainer Ludwig <ludwig@puresol-technologies.com>
2013-04-09 21:22:57 +0200

    Added three new views CorrelationChartView, HistorgramChartView and ParetoChartView to add the functionality the names of the views apply. Two different view references were removed from plugin.xml which were not implemented anymore.

---
+++ rcp/common/evaluation.plugin/src/com/puresol/coding/client/common/evaluation/views/HistorgramChartView.java
@@ -0,0 +1,20 @@
+package com.puresol.coding.client.common.evaluation.views;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+public class HistorgramChartView extends ViewPart {
+
+	@Override
+	public void createPartControl(Composite parent) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setFocus() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ rcp/common/evaluation.plugin/src/com/puresol/coding/client/common/evaluation/views/ParetoChartView.java
@@ -0,0 +1,20 @@
+package com.puresol.coding.client.common.evaluation.views;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+public class ParetoChartView extends ViewPart {
+
+	@Override
+	public void createPartControl(Composite parent) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setFocus() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
