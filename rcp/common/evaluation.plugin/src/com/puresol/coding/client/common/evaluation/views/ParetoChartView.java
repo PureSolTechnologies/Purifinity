51ef450fd46f89419cc54e709a4dd09e0c3e6da6
Rick-Rainer Ludwig <ludwig@puresol-technologies.com>
2013-04-14 18:04:32 +0200

    Removed BIRT dependencies.

--- rcp/common/evaluation.plugin/src/com/puresol/coding/client/common/evaluation/views/ParetoChartView.java
+++ rcp/common/evaluation.plugin/src/com/puresol/coding/client/common/evaluation/views/ParetoChartView.java
@@ -1,101 +1,71 @@
 package com.puresol.coding.client.common.evaluation.views;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.eclipse.birt.chart.model.ChartWithAxes;
-import org.eclipse.birt.chart.model.attribute.ChartDimension;
-import org.eclipse.birt.chart.model.attribute.LegendItemType;
-import org.eclipse.birt.chart.model.attribute.impl.ColorDefinitionImpl;
-import org.eclipse.birt.chart.model.component.Axis;
-import org.eclipse.birt.chart.model.component.Series;
-import org.eclipse.birt.chart.model.component.impl.SeriesImpl;
-import org.eclipse.birt.chart.model.data.NumberDataSet;
-import org.eclipse.birt.chart.model.data.SeriesDefinition;
-import org.eclipse.birt.chart.model.data.TextDataSet;
-import org.eclipse.birt.chart.model.data.impl.NumberDataSetImpl;
-import org.eclipse.birt.chart.model.data.impl.SeriesDefinitionImpl;
-import org.eclipse.birt.chart.model.data.impl.TextDataSetImpl;
-import org.eclipse.birt.chart.model.impl.ChartWithAxesImpl;
-import org.eclipse.birt.chart.model.layout.Plot;
-import org.eclipse.birt.chart.model.type.BarSeries;
-import org.eclipse.birt.chart.model.type.impl.BarSeriesImpl;
 import org.eclipse.jface.action.IToolBarManager;
 import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.RGB;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.ISelectionListener;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.part.ViewPart;
 
+import com.puresol.coding.client.common.chart.Axis;
+import com.puresol.coding.client.common.chart.AxisDirection;
+import com.puresol.coding.client.common.chart.AxisFactory;
+import com.puresol.coding.client.common.chart.Chart2D;
+import com.puresol.coding.client.common.chart.ChartCanvas;
+import com.puresol.coding.client.common.chart.DataPoint2D;
+import com.puresol.coding.client.common.chart.Plot;
+import com.puresol.coding.client.common.chart.math.Point2D;
+import com.puresol.coding.client.common.chart.renderer.BarMarkRenderer;
+import com.puresol.coding.client.common.chart.renderer.ConstantColorProvider;
 import com.puresol.coding.client.common.ui.actions.PartSettingsCapability;
 import com.puresol.coding.client.common.ui.actions.RefreshAction;
 import com.puresol.coding.client.common.ui.actions.Refreshable;
 import com.puresol.coding.client.common.ui.actions.Reproducable;
 import com.puresol.coding.client.common.ui.actions.ShowSettingsAction;
 import com.puresol.coding.client.common.ui.actions.ViewReproductionAction;
-import com.puresol.coding.client.common.ui.components.ChartCanvas;
+import com.puresol.coding.metrics.maintainability.MaintainabilityIndexEvaluatorParameter;
+import com.puresol.utils.math.LevelOfMeasurement;
+import com.puresol.utils.math.ParameterWithArbitraryUnit;
 
 public class ParetoChartView extends ViewPart implements Refreshable,
 		Reproducable, ISelectionListener, PartSettingsCapability {
 
 	private ChartCanvas chartCanvas;
-	private ChartWithAxes chart;
 
 	@Override
 	public void createPartControl(Composite parent) {
 		parent.setLayout(new FillLayout());
-		chartCanvas = new ChartCanvas(parent);
-		chart = ChartWithAxesImpl.create();
-		chartCanvas.setChart(chart);
+		chartCanvas = new ChartCanvas(parent, SWT.NONE);
+		Chart2D chart = new Chart2D();
+		chart.setTitle("Pareto Chart SLOC Evaluator");
+		chart.setSubTitle("PhyLOC");
 
-		chart.setDimension(ChartDimension.TWO_DIMENSIONAL_LITERAL);
-		Plot plot = chart.getPlot();
-		plot.setBackground(ColorDefinitionImpl.CREAM());
-		plot.getClientArea().setBackground(ColorDefinitionImpl.WHITE());
-		chart.getLegend().setItemType(LegendItemType.CATEGORIES_LITERAL);
-		chart.getLegend().setVisible(true);
-		chart.getTitle().getLabel().getCaption().setValue("Pareto Chart");
-		chart.getTitle().getLabel().getCaption().getFont().setSize(14);
-		chart.getTitle().getLabel().getCaption().getFont().setName("Arial");
+		Axis<String> xAxis = AxisFactory.createCategoryAxis(AxisDirection.X,
+				new ParameterWithArbitraryUnit<String>("File", "",
+						LevelOfMeasurement.NOMINAL, "", String.class), "File1",
+				"File2", "File3", "File4", "File5");
+		chart.setxAxis(xAxis);
 
-		Axis xAxis = chart.getPrimaryBaseAxes()[0];
-		xAxis.getTitle().setVisible(true);
-		xAxis.getTitle().getCaption().setValue("X-Axis");
-		xAxis.setGapWidth(0);
+		Axis<Double> yAxis = AxisFactory.createDoubleValueAxis(AxisDirection.Y,
+				MaintainabilityIndexEvaluatorParameter.MI, 0, 200, 20, 3);
+		chart.setyAxis(yAxis);
 
-		Axis yAxis = chart.getPrimaryOrthogonalAxis(xAxis);
-		yAxis.getTitle().setVisible(true);
-		yAxis.getTitle().getCaption().setValue("Y-Axis");
-		yAxis.getScale().setStep(1000.0);
+		Plot<String, Double> plot = new Plot<String, Double>(xAxis, yAxis,
+				"Correlation");
+		plot.add(new DataPoint2D(new Point2D(0.0, 5)));
+		plot.add(new DataPoint2D(new Point2D(1.0, 25)));
+		plot.add(new DataPoint2D(new Point2D(2.0, 50)));
+		plot.add(new DataPoint2D(new Point2D(3.0, 20)));
+		plot.add(new DataPoint2D(new Point2D(4.0, 10)));
+		chart.addPlot(plot);
 
-		List<String> files = new ArrayList<String>();
-		List<Double> values = new ArrayList<Double>();
-		for (int i = 0; i < 100; i++) {
-			files.add("File" + i);
-			values.add(Math.pow(100.0 - i, 2.0));
-		}
-
-		TextDataSet categoryValues = TextDataSetImpl.create(files
-				.toArray(new String[files.size()]));
-
-		Series seCategory = SeriesImpl.create();
-		seCategory.setDataSet(categoryValues);
-
-		SeriesDefinition sdX = SeriesDefinitionImpl.create();
-
-		xAxis.getSeriesDefinitions().add(sdX);
-		sdX.getSeries().add(seCategory);
-
-		NumberDataSet orthoValuesDataSet1 = NumberDataSetImpl.create(values
-				.toArray(new Double[values.size()]));
-
-		BarSeries bs1 = (BarSeries) BarSeriesImpl.create();
-		bs1.setDataSet(orthoValuesDataSet1);
-
-		SeriesDefinition sdY = SeriesDefinitionImpl.create();
-		yAxis.getSeriesDefinitions().add(sdY);
-		sdY.getSeries().add(bs1);
+		chartCanvas.setChart2D(chart);
+		chartCanvas.setMarkRenderer(plot, new BarMarkRenderer(1.0));
+		chartCanvas.setColorProvider(plot, new ConstantColorProvider(new RGB(
+				255, 0, 0), new RGB(0, 0, 255)));
 
 		// TODO Auto-generated method stub
 		initializeToolBar();
