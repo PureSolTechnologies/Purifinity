e1459693b41c1f729d392ff806b9414d5a62aff3
Rick-Rainer Ludwig <ludwig@puresol-technologies.com>
2013-04-19 22:26:49 +0200

    Got first version of correlation plot working.

--- rcp/common/evaluation.plugin/src/com/puresol/coding/client/common/evaluation/views/CorrelationChartView.java
+++ rcp/common/evaluation.plugin/src/com/puresol/coding/client/common/evaluation/views/CorrelationChartView.java
@@ -1,11 +1,15 @@
 package com.puresol.coding.client.common.evaluation.views;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.eclipse.jface.action.IToolBarManager;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Composite;
 
 import com.puresol.coding.analysis.api.HashIdFileTree;
+import com.puresol.coding.client.common.analysis.views.FileAnalysisSelection;
 import com.puresol.coding.client.common.chart.Axis;
 import com.puresol.coding.client.common.chart.AxisDirection;
 import com.puresol.coding.client.common.chart.AxisFactory;
@@ -14,13 +18,21 @@ import com.puresol.coding.client.common.chart.ChartCanvas;
 import com.puresol.coding.client.common.chart.DataPoint2D;
 import com.puresol.coding.client.common.chart.Plot;
 import com.puresol.coding.client.common.chart.math.Point2D;
+import com.puresol.coding.client.common.chart.renderer.CircleMarkRenderer;
 import com.puresol.coding.client.common.evaluation.CorrelationChartViewSettingsDialog;
 import com.puresol.coding.client.common.ui.actions.RefreshAction;
 import com.puresol.coding.client.common.ui.actions.ShowSettingsAction;
 import com.puresol.coding.client.common.ui.actions.ViewReproductionAction;
 import com.puresol.coding.evaluation.api.EvaluatorFactory;
+import com.puresol.coding.evaluation.api.EvaluatorStore;
+import com.puresol.coding.evaluation.api.EvaluatorStoreFactory;
+import com.puresol.coding.evaluation.api.MetricFileResults;
 import com.puresol.coding.metrics.maintainability.MaintainabilityIndexEvaluatorParameter;
 import com.puresol.coding.metrics.sloc.SLOCEvaluatorParameter;
+import com.puresol.trees.TreeVisitor;
+import com.puresol.trees.TreeWalker;
+import com.puresol.trees.WalkingAction;
+import com.puresol.utils.HashId;
 import com.puresol.utils.math.Parameter;
 
 public class CorrelationChartView extends AbstractMetricViewPart {
@@ -32,13 +44,14 @@ public class CorrelationChartView extends AbstractMetricViewPart {
 	private EvaluatorFactory yMetricSelection = null;
 	private Parameter<?> yParameterSelection = null;
 
+	private Chart2D chart;
 	private ChartCanvas chartCanvas;
 
 	@Override
 	public void createPartControl(Composite parent) {
 		parent.setLayout(new FillLayout());
 		chartCanvas = new ChartCanvas(parent, SWT.NONE);
-		Chart2D chart = new Chart2D();
+		chart = new Chart2D();
 		chart.setTitle("Metrics Correlation");
 		chart.setSubTitle("SLOC <-> Maintainability Index");
 
@@ -117,15 +130,99 @@ public class CorrelationChartView extends AbstractMetricViewPart {
 	}
 
 	@Override
-	public void showEvaluation(HashIdFileTree path) {
-		// TODO Auto-generated method stub
-
+	protected void updateEvaluation() {
+		FileAnalysisSelection analysisSelection = getAnalysisSelection();
+		if ((analysisSelection != null) && (xMetricSelection != null)
+				&& (xParameterSelection != null) && (yMetricSelection != null)
+				&& (yParameterSelection != null)) {
+			HashIdFileTree path = analysisSelection.getHashIdFile();
+			if (path.isFile()) {
+				path = path.getParent();
+			}
+			showEvaluation(path);
+		}
 	}
 
 	@Override
-	protected void updateEvaluation() {
-		// TODO Auto-generated method stub
+	public void showEvaluation(HashIdFileTree path) {
+		final EvaluatorStore xStore = EvaluatorStoreFactory.getFactory()
+				.createInstance(xMetricSelection.getEvaluatorClass());
+		final EvaluatorStore yStore = EvaluatorStoreFactory.getFactory()
+				.createInstance(yMetricSelection.getEvaluatorClass());
+		final Map<Double, Double> correlationValues = new HashMap<Double, Double>();
+		TreeVisitor<HashIdFileTree> visitor = new TreeVisitor<HashIdFileTree>() {
+			@Override
+			public WalkingAction visit(HashIdFileTree node) {
+				if (!node.isFile()) {
+					return WalkingAction.PROCEED;
+				}
+				HashId hashId = node.getHashId();
+				MetricFileResults xResults = xStore.readFileResults(hashId);
+				if (xResults == null) {
+					return WalkingAction.PROCEED;
+				}
+				double xValue = findSuitableValue(node, xResults,
+						xParameterSelection);
+				MetricFileResults yResults = yStore.readFileResults(hashId);
+				if (yResults == null) {
+					return WalkingAction.PROCEED;
+				}
+				double yValue = findSuitableValue(node, yResults,
+						yParameterSelection);
+				correlationValues.put(xValue, yValue);
+				return WalkingAction.PROCEED;
+			}
+		};
+		TreeWalker.walk(visitor, path);
 
+		setupChart(correlationValues);
 	}
 
+	private void setupChart(final Map<Double, Double> correlationValues) {
+		chart.removeAllPlots();
+
+		chart.setTitle("Correlation Chart for " + xMetricSelection.getName()
+				+ " and " + yMetricSelection.getName());
+		chart.setSubTitle(xParameterSelection.getName() + "<->"
+				+ yParameterSelection.getName());
+
+		double xMin = 0.0;
+		double xMax = 0.0;
+		for (Double value : correlationValues.keySet()) {
+			xMin = Math.min(xMin, value);
+			xMax = Math.max(xMax, value);
+		}
+
+		double yMin = 0.0;
+		double yMax = 0.0;
+		for (Double value : correlationValues.values()) {
+			yMin = Math.min(yMin, value);
+			yMax = Math.max(yMax, value);
+		}
+
+		@SuppressWarnings("unchecked")
+		Parameter<Double> xAxisParameter = (Parameter<Double>) xParameterSelection;
+		Axis<Double> xAxis = AxisFactory.createDoubleValueAxis(AxisDirection.X,
+				xAxisParameter, xMin, xMax, (xMax - xMin) / 10.0, 1);
+		chart.setxAxis(xAxis);
+
+		@SuppressWarnings("unchecked")
+		Parameter<Double> yAxisParameter = (Parameter<Double>) yParameterSelection;
+		Axis<Double> yAxis = AxisFactory.createDoubleValueAxis(AxisDirection.Y,
+				yAxisParameter, yMin, yMax, (yMax - yMin) / 10.0, 1);
+		chart.setyAxis(yAxis);
+
+		Plot<Double, Double> plot = new Plot<Double, Double>(xAxis, yAxis,
+				"Correlation Plot");
+		for (Double x : correlationValues.keySet()) {
+			plot.add(new DataPoint2D(new Point2D(x, correlationValues.get(x))));
+		}
+		chart.addPlot(plot);
+		chartCanvas.setMarkRenderer(plot, new CircleMarkRenderer());
+
+		// ChartConfigProvider configProvider = getConfigProvider();
+		// chartCanvas.setColorProvider(plot,
+		// configProvider.getColorProvider());
+		chartCanvas.refresh();
+	}
 }
