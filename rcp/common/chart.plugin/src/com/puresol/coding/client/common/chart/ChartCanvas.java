7fae613b50e47120ad52c0c23c43785a7867ef7e
Rick-Rainer Ludwig <ludwig@puresol-technologies.com>
2013-04-25 23:11:04 +0200

    Refactoring of chart rendering.

--- rcp/common/chart.plugin/src/com/puresol/coding/client/common/chart/ChartCanvas.java
+++ rcp/common/chart.plugin/src/com/puresol/coding/client/common/chart/ChartCanvas.java
@@ -1,63 +1,64 @@
 package com.puresol.coding.client.common.chart;
 
-import org.eclipse.swt.SWT;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.eclipse.jface.window.DefaultToolTip;
+import org.eclipse.swt.events.PaintEvent;
+import org.eclipse.swt.events.PaintListener;
 import org.eclipse.swt.graphics.Color;
-import org.eclipse.swt.graphics.Font;
+import org.eclipse.swt.graphics.GC;
+import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.graphics.RGB;
-import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.widgets.Canvas;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Event;
 
+import com.puresol.coding.client.common.chart.math.TransformationMatrix2D;
+import com.puresol.coding.client.common.chart.renderer.AxisRenderer;
 import com.puresol.coding.client.common.chart.renderer.ColorProvider;
 import com.puresol.coding.client.common.chart.renderer.MarkRenderer;
+import com.puresol.coding.client.common.chart.renderer.ChartRenderer;
 
 /**
  * This is the central canvas class for interactive charting.
  * 
  * @author Rick-Rainer Ludwig
  */
-public class ChartCanvas extends Canvas {
+public class ChartCanvas extends Canvas implements PaintListener {
 
-	private final Label title;
-	private final Label subTitle;
-	private final PlotCanvas plot;
-	// private final Canvas legende;
-	private final Font titleFont;
-	private final Font subTitleFont;
+	private static final int MARGIN = 5;
 
-	private Chart2D chart2D;
+	private final Map<Plot<?, ?>, List<MarkPosition>> markPositions = new HashMap<Plot<?, ?>, List<MarkPosition>>();
+
+	private final Map<Plot<?, ?>, MarkRenderer> markRenderers = new HashMap<Plot<?, ?>, MarkRenderer>();
+	private final Map<Plot<?, ?>, ColorProvider> colorProviders = new HashMap<Plot<?, ?>, ColorProvider>();
+
+	private Chart2D chart2D = null;
 
 	public ChartCanvas(Composite parent, int style) {
 		super(parent, style);
-		FillLayout layout = new FillLayout();
-		layout.marginHeight = 0;
-		layout.marginWidth = 0;
-		parent.setLayout(layout);
 
-		setLayout(new ChartLayout());
 		setBackground(new Color(getDisplay(), new RGB(255, 255, 255)));
-		title = new Label(this, SWT.NONE);
-		title.setLayoutData(ChartElement.TITLE);
-		title.setAlignment(SWT.CENTER);
-		Font defaultFont = title.getFont();
-		titleFont = new Font(title.getDisplay(),
-				defaultFont.getFontData()[0].getName(),
-				(int) (defaultFont.getFontData()[0].getHeight() * 1.5),
-				defaultFont.getFontData()[0].getStyle() | SWT.BOLD);
-		title.setFont(titleFont);
-		subTitle = new Label(this, SWT.NONE);
-		subTitle.setLayoutData(ChartElement.SUBTITLE);
-		subTitle.setAlignment(SWT.CENTER);
-		subTitleFont = new Font(title.getDisplay(),
-				defaultFont.getFontData()[0].getName(),
-				(int) (defaultFont.getFontData()[0].getHeight() * 1.2),
-				defaultFont.getFontData()[0].getStyle() | SWT.BOLD);
-		subTitle.setFont(subTitleFont);
-		plot = new PlotCanvas(this, SWT.BORDER);
-		plot.setLayoutData(ChartElement.PLOT);
-		// legende = new Canvas(canvas, SWT.BORDER);
-		// legende.setLayoutData(ChartElement.LEGENDE);
+
+		addPaintListener(this);
+		DefaultToolTip toolTip = new DefaultToolTip(this) {
+			@Override
+			protected String getText(Event event) {
+				return getTooltipText(event.x, event.y);
+			}
+
+			@Override
+			protected boolean shouldCreateToolTip(Event event) {
+				return getTooltipText(event.x, event.y) != null;
+			}
+		};
+		toolTip.setHideDelay(0);
+		toolTip.setPopupDelay(0);
+		toolTip.setShift(new Point(10, 10));
+
 	}
 
 	public Chart2D getChart2D() {
@@ -66,26 +67,118 @@ public class ChartCanvas extends Canvas {
 
 	public void setChart2D(Chart2D chart2D) {
 		this.chart2D = chart2D;
-		plot.setChart(chart2D);
 		refresh();
 	}
 
 	public void setMarkRenderer(Plot<?, ?> plot, MarkRenderer markRenderer) {
-		this.plot.setMarkRenderer(plot, markRenderer);
+		markRenderers.put(plot, markRenderer);
 	}
 
 	public void setColorProvider(Plot<?, ?> plot, ColorProvider colorProvider) {
-		this.plot.setColorProvider(plot, colorProvider);
+		colorProviders.put(plot, colorProvider);
+	}
+
+	public String getTooltipText(int x, int y) {
+		for (Plot<?, ?> plot : markPositions.keySet()) {
+			for (MarkPosition markPosition : markPositions.get(plot)) {
+				Rectangle position = markPosition.getPosition();
+				if ((x >= position.x) && (y >= position.y)
+						&& (x <= position.x + position.width)
+						&& (y <= position.y + position.height)) {
+					int index = markPosition.getDataPointIndex();
+					DataPoint2D<?, ?> dataPoint2D = plot.getDataPoints().get(
+							index);
+					String text = dataPoint2D.getX().toString() + " : "
+							+ dataPoint2D.getY().toString();
+					String remark = dataPoint2D.getRemark();
+					if ((remark != null)
+							&& (!dataPoint2D.getRemark().isEmpty())) {
+						text += " (" + dataPoint2D.getRemark() + ")";
+					}
+					return text;
+				}
+			}
+		}
+		return null;
+	}
+
+	@Override
+	public void paintControl(PaintEvent e) {
+		markPositions.clear();
+		if ((chart2D == null) || (chart2D.getXAxis() == null)
+				|| (chart2D.getYAxis() == null)) {
+			return;
+		}
+		GC gc = e.gc;
+		Rectangle clientArea = getClientArea();
+		if ((clientArea.width <= 2 * MARGIN || (clientArea.height <= 2 * MARGIN))) {
+			return;
+		}
+
+		clientArea.x += MARGIN;
+		clientArea.y += MARGIN;
+		clientArea.width -= 2 * MARGIN;
+		clientArea.height -= 2 * MARGIN;
+
+		// Initialize renderer objects...
+		AxisRenderer xAxisRenderer = new AxisRenderer(gc, chart2D.getXAxis(),
+				chart2D.getYAxis().getMinimum());
+		AxisRenderer yAxisRenderer = new AxisRenderer(gc, chart2D.getYAxis(),
+				chart2D.getXAxis().getMinimum());
+		int yAxisWidth = yAxisRenderer.getWidth();
+		clientArea.x += yAxisWidth;
+		clientArea.width -= yAxisWidth;
+		int xAxisWidth = xAxisRenderer.getWidth();
+		clientArea.height -= xAxisWidth;
+
+		/*
+		 * Due to the missing fractional pixel drawing facilities in Eclipse we
+		 * need to transform everything on our own. The transformation matrix is
+		 * created to have a mathematical correct drawing.
+		 */
+		TransformationMatrix2D transformMatrix2d = new TransformationMatrix2D();
+		// move origin to canvas center
+		transformMatrix2d.translate(clientArea.x + clientArea.width / 2,
+				clientArea.y + clientArea.height / 2);
+		// let y axis point upright
+		transformMatrix2d.mirror(AxisDirection.X);
+
+		Axis<?> xAxis = chart2D.getXAxis();
+		Axis<?> yAxis = chart2D.getYAxis();
+
+		double rangeX = xAxis.getMaximum() - xAxis.getMinimum();
+		double rangeY = yAxis.getMaximum() - yAxis.getMinimum();
+
+		// scale to use the values from dataset to paint
+		double scaleX = clientArea.width / rangeX;
+		double scaleY = clientArea.height / rangeY;
+		transformMatrix2d.scale(scaleX, scaleY);
+
+		// move the center of axes to center of canvas
+		double centerX = (xAxis.getMinimum() + xAxis.getMaximum()) / 2.0;
+		double centerY = (yAxis.getMinimum() + yAxis.getMaximum()) / 2.0;
+		transformMatrix2d.translate(-centerX, -centerY);
+
+		for (Plot<?, ?> plot : chart2D.getPlots()) {
+			ChartRenderer plotRenderer = new ChartRenderer(gc, plot);
+			MarkRenderer markRenderer = markRenderers.get(plot);
+			if (markRenderer != null) {
+				plotRenderer.setMarkRenderer(markRenderer);
+			}
+			ColorProvider colorProvider = colorProviders.get(plot);
+			if (colorProvider != null) {
+				plotRenderer.setColorProvider(colorProvider);
+			}
+			List<MarkPosition> positions = plotRenderer
+					.render(transformMatrix2d);
+			markPositions.put(plot, positions);
+		}
+
+		xAxisRenderer.render(transformMatrix2d);
+		yAxisRenderer.render(transformMatrix2d);
 	}
 
 	public void refresh() {
-		if (chart2D.getTitle() != null) {
-			title.setText(chart2D.getTitle());
-		}
-		if (chart2D.getSubTitle() != null) {
-			subTitle.setText(chart2D.getSubTitle());
-		}
-		plot.redraw();
 		layout();
 		redraw();
 		update();
--- rcp/common/chart.plugin/src/com/puresol/coding/client/common/chart/renderer/ChartRenderer.java
+++ rcp/common/chart.plugin/src/com/puresol/coding/client/common/chart/renderer/ChartRenderer.java
@@ -6,32 +6,51 @@ import java.util.List;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.GC;
 import org.eclipse.swt.graphics.RGB;
+import org.eclipse.swt.graphics.Rectangle;
 
-import com.puresol.coding.client.common.chart.Chart2D;
 import com.puresol.coding.client.common.chart.ColoredArea;
+import com.puresol.coding.client.common.chart.DataPoint2D;
+import com.puresol.coding.client.common.chart.MarkPosition;
+import com.puresol.coding.client.common.chart.Plot;
 import com.puresol.coding.client.common.chart.math.Point2D;
 import com.puresol.coding.client.common.chart.math.TransformationMatrix2D;
 
 public class ChartRenderer {
 
-	private final Chart2D chart;
+	private final GC gc;
+	private final Plot<?, ?> plot;
+	private MarkRenderer markRenderer = new CrossMarkRenderer();
+	private ColorProvider colorProvider;
 
-	private final List<ColoredArea> coloredAreas = new ArrayList<ColoredArea>();
-
-	public ChartRenderer(Chart2D chart) {
-		this.chart = chart;
+	public ChartRenderer(GC gc, Plot<?, ?> plot) {
+		this.gc = gc;
+		this.plot = plot;
 	}
 
-	public void addColoredArea(ColoredArea coloredArea) {
-		coloredAreas.add(coloredArea);
+	public MarkRenderer getMarkRenderer() {
+		return markRenderer;
 	}
 
-	public List<ColoredArea> getColoredAreas() {
-		return coloredAreas;
+	public void setMarkRenderer(MarkRenderer markRenderer) {
+		this.markRenderer = markRenderer;
 	}
 
-	public void render(GC gc, TransformationMatrix2D transformation) {
-		for (ColoredArea coloredArea : coloredAreas) {
+	public ColorProvider getColorProvider() {
+		return colorProvider;
+	}
+
+	public void setColorProvider(ColorProvider colorProvider) {
+		this.colorProvider = colorProvider;
+	}
+
+	public List<MarkPosition> render(TransformationMatrix2D transformation) {
+		renderColoredAreas(transformation);
+		List<MarkPosition> markPositions = renderMarks(transformation);
+		return markPositions;
+	}
+
+	private void renderColoredAreas(TransformationMatrix2D transformation) {
+		for (ColoredArea<?, ?> coloredArea : plot.getColoredAreas()) {
 			RGB rgb = coloredArea.getColor();
 			Color color = new Color(gc.getDevice(), rgb);
 			try {
@@ -49,6 +68,44 @@ public class ChartRenderer {
 				color.dispose();
 			}
 		}
+	}
+
+	private List<MarkPosition> renderMarks(TransformationMatrix2D transformation) {
+		Color currentForeground = gc.getForeground();
+		Color currentBackground = gc.getBackground();
+		List<MarkPosition> markPositions = new ArrayList<MarkPosition>();
+		List<?> dataPoints = plot.getDataPoints();
+		for (int i = 0; i < dataPoints.size(); i++) {
+			DataPoint2D<?, ?> dataPoint = (DataPoint2D<?, ?>) dataPoints.get(i);
+			Color foreground = null;
+			Color background = null;
+			if (colorProvider != null) {
+				Object y = dataPoint.getY();
+				RGB foregroundRGB = colorProvider.getForegroundColor(y);
+				if (foregroundRGB != null) {
+					foreground = new Color(gc.getDevice(), foregroundRGB);
+					gc.setForeground(foreground);
+				}
+				RGB backgroundRGB = colorProvider.getBackgroundColor(y);
+				if (backgroundRGB != null) {
+					background = new Color(gc.getDevice(), backgroundRGB);
+					gc.setBackground(background);
+				}
+
+			}
+			Rectangle markPosition = markRenderer.render(gc, transformation,
+					plot, dataPoint.getX(), dataPoint.getY());
+			markPositions.add(new MarkPosition(plot, i, markPosition));
+			if (foreground != null) {
+				foreground.dispose();
+			}
+			if (background != null) {
+				background.dispose();
+			}
+		}
+		gc.setForeground(currentForeground);
+		gc.setBackground(currentBackground);
+		return markPositions;
 	}
 
 }
