0f8835c9f6584f4ca4ffff4d62931489015a62b8
Rick-Rainer Ludwig <ludwig@puresol-technologies.com>
2013-05-04 22:34:00 +0200

    Fixed AnalysisRunImplTest which was broken due to missing SPI entry for AnalysisStoreFactory.

--- base/coding/store/fs/lib/src/test/java/com/puresol/coding/store/fs/analysis/AnalysisRunImplTest.java
+++ base/coding/store/fs/lib/src/test/java/com/puresol/coding/store/fs/analysis/AnalysisRunImplTest.java
@@ -27,47 +27,47 @@ import com.puresol.utils.HashId;
 
 public class AnalysisRunImplTest {
 
-    private static AnalysisStore analysisStore;
-    private static AnalysisProject analysis;
+	private static AnalysisStore analysisStore;
+	private static AnalysisProject analysis;
 
-    @BeforeClass
-    public static void initialize() throws AnalysisStoreException {
-	analysisStore = AnalysisStoreFactory.getFactory().getInstance();
-	analysis = analysisStore.createAnalysis(new AnalysisProjectSettings(
-		"Name", "Description", new TestFileSearchConfiguration(),
-		new DirectoryRepositoryLocation("src", new File("src"))));
-	assertNotNull(analysis);
-    }
+	@BeforeClass
+	public static void initialize() throws AnalysisStoreException {
+		analysisStore = AnalysisStoreFactory.getFactory().getInstance();
+		analysis = analysisStore.createAnalysis(new AnalysisProjectSettings(
+				"Name", "Description", new TestFileSearchConfiguration(),
+				new DirectoryRepositoryLocation("src", new File("src"))));
+		assertNotNull(analysis);
+	}
 
-    @Test
-    @Ignore("For test, we do not have a bundle context!")
-    public void test() throws Exception {
-	AnalysisRun analysisRun = analysis.createAnalysisRun();
-	assertNotNull(analysisRun);
-	List<AnalyzedCode> analyzedFiles = analysisRun.getAnalyzedCodes();
-	assertNotNull(analyzedFiles);
-	List<AnalyzedCode> failedFiles = analysisRun.getFailedCodes();
-	assertNotNull(failedFiles);
-	HashIdFileTree fileTree = analysisRun.getFileTree();
-	assertNotNull(fileTree);
-	AnalysisRunInformation information = analysisRun.getInformation();
-	assertNotNull(information);
-    }
+	@Test
+	@Ignore("For test, we do not have a bundle context!")
+	public void test() throws Exception {
+		AnalysisRun analysisRun = analysis.createAnalysisRun();
+		assertNotNull(analysisRun);
+		List<AnalyzedCode> analyzedFiles = analysisRun.getAnalyzedCodes();
+		assertNotNull(analyzedFiles);
+		List<AnalyzedCode> failedFiles = analysisRun.getFailedCodes();
+		assertNotNull(failedFiles);
+		HashIdFileTree fileTree = analysisRun.getFileTree();
+		assertNotNull(fileTree);
+		AnalysisRunInformation information = analysisRun.getInformation();
+		assertNotNull(information);
+	}
 
-    @Test
-    public void testGetFileStoreDirectory() {
-	File fileStoreDirectory = FileStoreImpl.getFileDirectory(new HashId(
-		HashAlgorithm.SHA256, "1234567890"));
-	assertTrue(fileStoreDirectory.getPath().endsWith("/12/34/567890"));
-    }
+	@Test
+	public void testGetFileStoreDirectory() {
+		File fileStoreDirectory = FileStoreImpl.getFileDirectory(new HashId(
+				HashAlgorithm.SHA256, "1234567890"));
+		assertTrue(fileStoreDirectory.getPath().endsWith("/12/34/567890"));
+	}
 
-    @AfterClass
-    public static void destroy() throws AnalysisStoreException {
-	assertNotNull(analysisStore);
-	assertNotNull(analysis);
-	analysisStore.removeAnalysis(analysis.getInformation().getUUID());
-	analysis = null;
-	analysisStore = null;
-    }
+	@AfterClass
+	public static void destroy() throws AnalysisStoreException {
+		assertNotNull(analysisStore);
+		assertNotNull(analysis);
+		analysisStore.removeAnalysis(analysis.getInformation().getUUID());
+		analysis = null;
+		analysisStore = null;
+	}
 
 }
