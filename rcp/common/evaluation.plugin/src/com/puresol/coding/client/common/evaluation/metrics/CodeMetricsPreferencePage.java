ce7eac52de8b1a903ff89954676e0b90eca186eb
Rick-Rainer Ludwig <ludwig@puresol-technologies.com>
2013-05-05 17:11:59 +0200

    Added empty preference pages for metrics.

---
+++ rcp/common/evaluation.plugin/src/com/puresol/coding/client/common/evaluation/metrics/CodeMetricsPreferencePage.java
@@ -0,0 +1,37 @@
+package com.puresol.coding.client.common.evaluation.metrics;
+
+import org.eclipse.jface.preference.PreferencePage;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPreferencePage;
+
+public class CodeMetricsPreferencePage extends PreferencePage implements
+	IWorkbenchPreferencePage {
+
+    public CodeMetricsPreferencePage() {
+	super();
+    }
+
+    public CodeMetricsPreferencePage(String title) {
+	super(title);
+    }
+
+    public CodeMetricsPreferencePage(String title, ImageDescriptor image) {
+	super(title, image);
+    }
+
+    @Override
+    public void init(IWorkbench workbench) {
+	// TODO Auto-generated method stub
+
+    }
+
+    @Override
+    protected Control createContents(Composite parent) {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+}
