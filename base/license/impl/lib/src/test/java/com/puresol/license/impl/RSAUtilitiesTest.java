7308bc1edab05d274cc254966c0932592ba91d8d
Rick-Rainer Ludwig <ludwig@puresol-technologies.com>
2013-05-06 22:12:54 +0200

    Added License modules. These are empty except for impl.lib which contains demonstrators how to encrypt and decrypt files.

---
+++ base/license/impl/lib/src/test/java/com/puresol/license/impl/RSAUtilitiesTest.java
@@ -0,0 +1,50 @@
+package com.puresol.license.impl;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.security.InvalidParameterException;
+import java.security.KeyPair;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
+import org.junit.Test;
+
+import com.puresol.license.impl.RSAUtilities;
+
+public class RSAUtilitiesTest {
+
+    @Test(expected = InvalidParameterException.class)
+    public void testGenerateKeyPair511Bit() {
+	KeyPair keyPair = RSAUtilities.generateKeyPair(511);
+	assertNotNull(keyPair);
+    }
+
+    @Test
+    public void testGenerateKeyPair512Bit() {
+	KeyPair keyPair = RSAUtilities.generateKeyPair(512);
+	assertNotNull(keyPair);
+    }
+
+    @Test
+    public void testEncryptAndDecrypt() throws Exception {
+	KeyPair keyPair = RSAUtilities.generateKeyPair(512);
+
+	String message = "This is the secret message! ;-)";
+	System.out.println(message);
+
+	PublicKey publicKey = keyPair.getPublic();
+
+	byte[] encryptedMessage = RSAUtilities.encrypt(publicKey,
+		message.getBytes());
+	System.out.println(new String(encryptedMessage));
+
+	PrivateKey privateKey = keyPair.getPrivate();
+	byte[] decryptedMessage = RSAUtilities.decrypt(privateKey,
+		encryptedMessage);
+	String decryptedMessageString = new String(decryptedMessage);
+	System.out.println(decryptedMessageString);
+	assertEquals(message, decryptedMessageString);
+    }
+
+}
