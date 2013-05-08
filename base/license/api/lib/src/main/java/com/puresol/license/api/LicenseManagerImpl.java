c900ee2961a3b6d0b00a274e6dd28835195cb99c
Rick-Rainer Ludwig <ludwig@puresol-technologies.com>
2013-05-08 21:31:51 +0200

    Fixed test and expiration time check.

--- base/license/api/lib/src/main/java/com/puresol/license/api/LicenseManagerImpl.java
+++ base/license/api/lib/src/main/java/com/puresol/license/api/LicenseManagerImpl.java
@@ -10,25 +10,24 @@ import com.puresol.license.api.exception.LicenseException;
 
 public class LicenseManagerImpl implements LicenseManager {
 
-    @Override
-    public void checkIfLicensed(Class<?> clazz) throws LicenseException {
-	Date currentTime = new Date();
-	checkExpiration(currentTime);
-    }
-
-    void checkExpiration(Date currentTime) {
-	Calendar expireDate = GregorianCalendar.getInstance();
-	expireDate.set(2013, 5, 1, 0, 0);
-	if (expireDate.getTime().getTime() <= currentTime.getTime()) {
-	    throw new ExpiredLicenseException(
-		    "The license expired 2013-06-01. Please request for a new license.");
+	@Override
+	public void checkIfLicensed(Class<?> clazz) throws LicenseException {
+		Date currentTime = new Date();
+		checkExpiration(currentTime);
 	}
-    }
 
-    @Override
-    public void addLicenseFile(File licenseFile) {
-	// TODO Auto-generated method stub
+	void checkExpiration(Date currentTime) {
+		Calendar expireDate = new GregorianCalendar(2013, 5, 1, 0, 0, 0);
+		if (expireDate.getTime().getTime() <= currentTime.getTime()) {
+			throw new ExpiredLicenseException(
+					"The license expired 2013-06-01. Please request for a new license.");
+		}
+	}
 
-    }
+	@Override
+	public void addLicenseFile(File licenseFile) {
+		// TODO Auto-generated method stub
+
+	}
 
 }
