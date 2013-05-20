74539e87f696e5a2e31f5ec0fbe00bbc9521fd0e
Rick-Rainer Ludwig <ludwig@puresol-technologies.com>
2013-05-20 18:56:04 +0200

    Finished license manager in first version.

--- base/license/test/src/test/java/com/puresol/license/test/impl/LicenseFileTest.java
+++ base/license/test/src/test/java/com/puresol/license/test/impl/LicenseFileTest.java
@@ -1,5 +1,7 @@
 package com.puresol.license.test.impl;
 
+import static org.junit.Assert.assertEquals;
+
 import java.io.File;
 import java.security.KeyPair;
 import java.util.GregorianCalendar;
@@ -21,33 +23,33 @@ import com.puresol.utils.crypt.RSAUtilities;
 
 public class LicenseFileTest extends AbstractLicenseFacilityTest {
 
-	private static final File testLicenseFile = new File(
-			"target/licenses/testLicenseFile.license");
+    private static final File testLicenseFile = new File(
+	    "target/licenses/testLicenseFile.license");
 
-	private static Licensee testLicensee;
+    private static Licensee testLicensee;
 
-	@BeforeClass
-	public static void initialitzeTestData() throws LicenseStoreException {
-		KeyPair generatedKeyPair = RSAUtilities.generateKeyPair(512);
-		testLicensee = new Licensee("me", "PureSol Technologies");
-		TestFileSystemLicenseStore licenseStore = (TestFileSystemLicenseStore) getLicenseStore();
-		licenseStore.addLicensee(testLicensee, generatedKeyPair);
-	}
+    @BeforeClass
+    public static void initialitzeTestData() throws LicenseStoreException {
+	KeyPair generatedKeyPair = RSAUtilities.generateKeyPair(512);
+	testLicensee = new Licensee("me", "PureSol Technologies");
+	TestFileSystemLicenseStore licenseStore = (TestFileSystemLicenseStore) getLicenseStore();
+	licenseStore.addLicensee(testLicensee, generatedKeyPair);
+    }
 
-	@Test
-	public void testWriteAndRead() throws Exception {
-		GregorianCalendar expirationDate = new GregorianCalendar(2100, 0, 1);
-		LicenseManager licenseManager = getLicenseManager();
-		Product product = new Product("Purifinity", new Version(0, 8, 15));
-		Licenser licenser = new Licenser("PureSol Technologies",
-				"contact@puresol-technologies.com");
-		License license = licenseManager.createLicense(product, licenser,
-				testLicensee, expirationDate.getTime());
-		LicenseFile licenseFile = new LicenseFile(testLicenseFile);
-		KeyPair keyPair = getLicenseStore().getKeyPair(
-				license.getLicensee().getCustomerId());
-		licenseFile.writeLicense(license, keyPair.getPrivate());
-		License readLicense = licenseFile.readLicense();
-		// assertEquals(license, readLicense);
-	}
+    @Test
+    public void testWriteAndRead() throws Exception {
+	GregorianCalendar expirationDate = new GregorianCalendar(2100, 0, 1);
+	LicenseManager licenseManager = getLicenseManager();
+	Product product = new Product("Purifinity", new Version(0, 8, 15));
+	Licenser licenser = new Licenser("PureSol Technologies",
+		"contact@puresol-technologies.com");
+	License license = licenseManager.createLicense(product, licenser,
+		testLicensee, expirationDate.getTime());
+	LicenseFile licenseFile = new LicenseFile(testLicenseFile);
+	KeyPair keyPair = getLicenseStore().getKeyPair(
+		license.getLicensee().getCustomerId());
+	licenseFile.writeLicense(license, keyPair.getPrivate());
+	License readLicense = licenseFile.readLicense();
+	assertEquals(license, readLicense);
+    }
 }
