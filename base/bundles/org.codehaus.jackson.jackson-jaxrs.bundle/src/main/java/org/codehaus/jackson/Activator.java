7fa8cdf241a251ada78de1976c73ad6184869bb7
Rick-Rainer Ludwig <ludwig@puresol-technologies.com>
2013-08-10 17:16:48 +0200

    Added org.codehaus.jackson.jackson-jaxrs.bundle.

---
+++ base/bundles/org.codehaus.jackson.jackson-jaxrs.bundle/src/main/java/org/codehaus/jackson/Activator.java
@@ -0,0 +1,15 @@
+package org.codehaus.jackson;
+
+import org.osgi.framework.BundleContext;
+
+import com.puresol.commons.osgi.AbstractActivator;
+
+public class Activator
+
+extends AbstractActivator {
+
+	@Override
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+	}
+}
