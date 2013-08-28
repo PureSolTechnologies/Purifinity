7220248221b11486b151241ab70a671fffba22e5
Rick-Rainer Ludwig <ludwig@puresol-technologies.com>
2013-08-28 23:44:51 +0200

    Updated perspectives.

--- rcp/common/evaluation.plugin/src/com/puresol/purifinity/client/common/evaluation/perspectives/EvaluationPerspective.java
+++ rcp/common/evaluation.plugin/src/com/puresol/purifinity/client/common/evaluation/perspectives/EvaluationPerspective.java
@@ -7,32 +7,31 @@ import org.eclipse.ui.IPerspectiveFactory;
 import com.puresol.purifinity.client.common.analysis.views.AnalysisProjectsView;
 import com.puresol.purifinity.client.common.analysis.views.AnalysisRunContentView;
 import com.puresol.purifinity.client.common.analysis.views.AnalysisRunsView;
-import com.puresol.purifinity.client.common.analysis.views.AvailableLanguagesView;
-import com.puresol.purifinity.client.common.evaluation.views.DefectDensityMapView;
+import com.puresol.purifinity.client.common.evaluation.views.AvailableEvaluatorsView;
+import com.puresol.purifinity.client.common.evaluation.views.MetricsTableView;
 
 public class EvaluationPerspective implements IPerspectiveFactory {
 
-    @Override
-    public void createInitialLayout(IPageLayout layout) {
-	layout.setEditorAreaVisible(true);
+	@Override
+	public void createInitialLayout(IPageLayout layout) {
+		layout.setEditorAreaVisible(false);
 
-	layout.addView(AnalysisRunContentView.class.getName(),
-		IPageLayout.LEFT, 0.4f, layout.getEditorArea());
+		layout.addView(AnalysisRunContentView.class.getName(),
+				IPageLayout.LEFT, 0.4f, layout.getEditorArea());
 
-	layout.addView(AnalysisProjectsView.class.getName(), IPageLayout.LEFT,
-		0.5f, AnalysisRunContentView.class.getName());
+		layout.addView(AnalysisProjectsView.class.getName(), IPageLayout.LEFT,
+				0.3f, AnalysisRunContentView.class.getName());
 
-	layout.addView(AnalysisRunsView.class.getName(), IPageLayout.BOTTOM,
-		0.25f, AnalysisProjectsView.class.getName());
+		layout.addView(AnalysisRunsView.class.getName(), IPageLayout.BOTTOM,
+				0.25f, AnalysisProjectsView.class.getName());
 
-	layout.addView(DefectDensityMapView.class.getName(),
-		IPageLayout.BOTTOM, 0.6f,
-		AnalysisRunContentView.class.getName());
+		layout.addView(MetricsTableView.class.getName(), IPageLayout.RIGHT,
+				0.4f, AnalysisRunContentView.class.getName());
 
-	IFolderLayout folder = layout.createFolder("EvaluationMonitorAreas",
-		IPageLayout.BOTTOM, 0.75f, AnalysisRunsView.class.getName());
-	folder.addView(IPageLayout.ID_PROGRESS_VIEW);
-	folder.addView(AvailableLanguagesView.class.getName());
-    }
+		IFolderLayout folder = layout.createFolder("EvaluationMonitorAreas",
+				IPageLayout.BOTTOM, 0.5f, AnalysisRunsView.class.getName());
+		folder.addView(IPageLayout.ID_PROGRESS_VIEW);
+		folder.addView(AvailableEvaluatorsView.class.getName());
+	}
 
 }
