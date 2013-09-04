390681048de3af320453e95203024f65d759e899
Rick-Rainer Ludwig <ludwig@puresol-technologies.com>
2013-09-04 23:38:22 +0200

    Fixed intro, but did not really got grip...

--- rcp/application/plugin/src/com/puresol/purifinity/client/application/PurifinityIntroPart.java
+++ rcp/application/plugin/src/com/puresol/purifinity/client/application/PurifinityIntroPart.java
@@ -7,27 +7,27 @@ import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.part.IntroPart;
 
 public class PurifinityIntroPart extends IntroPart {
-    private Text text;
+	private Text text;
 
-    @Override
-    public void standbyStateChanged(boolean standby) {
-    }
+	@Override
+	public void standbyStateChanged(boolean standby) {
+	}
 
-    /**
-     * @wbp.parser.entryPoint
-     */
-    @Override
-    public void createPartControl(Composite parent) {
-	Composite composite = new Composite(parent, SWT.NONE);
-	composite.setLayout(new FillLayout(SWT.HORIZONTAL));
+	/**
+	 * @wbp.parser.entryPoint
+	 */
+	@Override
+	public void createPartControl(Composite parent) {
+		Composite composite = new Composite(parent, SWT.NONE);
+		composite.setLayout(new FillLayout(SWT.HORIZONTAL));
 
-	text = new Text(composite, SWT.BORDER);
-	text.setEditable(false);
-	text.setText("Welcome to Purifinity!\n\nIt is the tool No. 1 for source code analysis.");
-    }
+		text = new Text(composite, SWT.BORDER);
+		text.setEditable(false);
+		text.setText("Welcome to Purifinity!\n\nIt is the tool No. 1 for source code analysis.");
+	}
 
-    @Override
-    public void setFocus() {
-	text.setFocus();
-    }
+	@Override
+	public void setFocus() {
+		text.setFocus();
+	}
 }
