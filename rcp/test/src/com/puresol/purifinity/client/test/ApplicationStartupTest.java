e5666f3d3d1a30d09c529578002c969a42a4f425
Rick-Rainer Ludwig <ludwig@puresol-technologies.com>
2013-09-08 20:52:24 +0200

    Added client test module again. A test framework is needed.

---
+++ rcp/test/src/com/puresol/purifinity/client/test/ApplicationStartupTest.java
@@ -0,0 +1,10 @@
+package com.puresol.purifinity.client.test;
+
+/**
+ * http://www.vogella.com/articles/SWTBot/article.html
+ * 
+ * @author Rick-Rainer Ludwig
+ */
+public class ApplicationStartupTest {
+
+}
