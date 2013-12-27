7c8a4133f54a8ef3f21cac1de89a64ee5b4053c1
Rick-Rainer Ludwig <ludwig@puresol-technologies.com>
2013-12-27 20:26:06 +0100

    Some more work on store.

--- framework/store/db/lib/src/main/java/com/puresoltechnologies/purifinity/framework/store/db/analysis/DirectoryStoreImpl.java
+++ framework/store/db/lib/src/main/java/com/puresoltechnologies/purifinity/framework/store/db/analysis/DirectoryStoreImpl.java
@@ -1,6 +1,8 @@
 package com.puresoltechnologies.purifinity.framework.store.db.analysis;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import com.datastax.driver.core.ResultSet;
@@ -9,6 +11,9 @@ import com.puresoltechnologies.commons.misc.HashId;
 import com.puresoltechnologies.purifinity.framework.store.api.DirectoryStore;
 import com.puresoltechnologies.purifinity.framework.store.api.DirectoryStoreException;
 import com.puresoltechnologies.purifinity.framework.store.db.CassandraConnection;
+import com.puresoltechnologies.purifinity.framework.store.db.TitanConnection;
+import com.thinkaurelius.titan.core.TitanGraph;
+import com.tinkerpop.blueprints.Vertex;
 
 public class DirectoryStoreImpl implements DirectoryStore {
 
@@ -26,7 +31,37 @@ public class DirectoryStoreImpl implements DirectoryStore {
 
 	@Override
 	public List<HashId> getFiles(HashId hashId) throws DirectoryStoreException {
-		return restore(new File(getDirectoryStoreDirectory(hashId), FILES_FILE));
+		TitanGraph graph = TitanConnection.getGraph();
+		Vertex vertex = findTreeElement(graph, hashId);
+		Iterable<Vertex> vertices = vertex.query()
+				.has(TitanConnection.TREE_ELEMENT_IS_FILE, true).vertices();
+		Iterator<Vertex> vertexIterator = vertices.iterator();
+		List<HashId> hashIds = new ArrayList<>();
+		while (vertexIterator.hasNext()) {
+			Vertex fileVertex = vertexIterator.next();
+			hashIds.add(HashId.fromString((String) fileVertex
+					.getProperty(TitanConnection.TREE_ELEMENT_HASH)));
+		}
+		return hashIds;
+	}
+
+	private Vertex findTreeElement(TitanGraph graph, HashId hashId)
+			throws DirectoryStoreException {
+		Iterable<Vertex> vertices = graph.query()
+				.has(TitanConnection.TREE_ELEMENT_IS_FILE, false)
+				.has(TitanConnection.TREE_ELEMENT_HASH, hashId.getHash())
+				.vertices();
+		Iterator<Vertex> vertexIterator = vertices.iterator();
+		if (!vertexIterator.hasNext()) {
+			return null;
+		}
+		Vertex vertex = vertexIterator.next();
+		if (vertexIterator.hasNext()) {
+			throw new DirectoryStoreException(
+					"Found multiple directories with hash '" + hashId.getHash()
+							+ "'. Database is inconsistent!");
+		}
+		return vertex;
 	}
 
 	@Override
