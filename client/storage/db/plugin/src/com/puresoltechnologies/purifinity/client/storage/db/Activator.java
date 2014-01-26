50dea25d8cf7ec13c2f0cb7b8dad5457e779352b
Rick-Rainer Ludwig <ludwig@puresol-technologies.com>
2014-01-26 20:10:23 +0000

    Revert "Added cassandra-all to cassandra.bundle."
    This reverts commit 4c30b5db6e201384b72eea96a7220535a0a6edc7.

--- client/storage/db/plugin/src/com/puresoltechnologies/purifinity/client/storage/db/Activator.java
+++ client/storage/db/plugin/src/com/puresoltechnologies/purifinity/client/storage/db/Activator.java
@@ -1,22 +1,12 @@
 package com.puresoltechnologies.purifinity.client.storage.db;
 
-import java.io.IOException;
-
-import org.apache.cassandra.service.CassandraDaemon;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class Activator extends AbstractUIPlugin {
 
-	private static final Logger logger = LoggerFactory
-			.getLogger(Activator.class);
-
 	// The shared instance
 	private static Activator plugin = null;
-
-	private CassandraDaemon daemon = null;
 
 	@Override
 	public void start(BundleContext context) throws Exception {
@@ -40,25 +30,14 @@ public class Activator extends AbstractUIPlugin {
 		plugin = null;
 	}
 
-	private void startCassandra() throws IOException {
-		if (daemon != null) {
-			throw new RuntimeException("Cassandra was already initialized.");
-		}
-		daemon = new CassandraDaemon();
-		logger.info("Cassandra is about to start...");
-		daemon.init(null);
-		// daemon.start();
-		logger.info("Cassandra started.");
+	private void startCassandra() {
+		// TODO Auto-generated method stub
+
 	}
 
 	private void stopCassandra() {
-		if (daemon == null) {
-			throw new RuntimeException("Cassandra was never initialized.");
-		}
-		logger.info("Cassandra is about to stop...");
-		// daemon.stop();
-		daemon = null;
-		logger.info("Cassandra was stopped.");
+		// TODO Auto-generated method stub
+
 	}
 
 	/**
