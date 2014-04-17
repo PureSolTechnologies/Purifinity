4a0b06d711f079d789ab3888c1799b3733c1f3a8
Rick-Rainer Ludwig <ludwig@puresol-technologies.com>
2014-04-17 21:58:04 +0200

    Added purifinityserver.client.bundle and server.plugin for the client.

---
+++ server/purifinityserver/client.bundle/src/main/java/com/puresoltechnologies/purifinity/server/purifinityserver/client/Activator.java
@@ -0,0 +1,6 @@
+package com.puresoltechnologies.purifinity.server.purifinityserver.client;
+
+import com.puresoltechnologies.commons.osgi.AbstractActivator;
+
+public class Activator extends AbstractActivator {
+}
