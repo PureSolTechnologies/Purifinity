6139358e69c36ea04436c43440edf240d7fd332a
Rick-Rainer Ludwig <ludwig@puresol-technologies.com>
2015-01-22 19:51:53 +0100

    Work on Genesis integration. Tracker was added.

---
+++ server/passwordstore/ddl/src/main/java/com/puresoltechnologies/purifinity/server/passwordstore/ddl/PasswordStoreDatabaseMigrator.java
@@ -0,0 +1,43 @@
+package com.puresoltechnologies.purifinity.server.passwordstore.ddl;
+
+import com.puresoltechnologies.genesis.controller.GenesisController;
+
+/**
+ * This migrator starts all services of all services included in Purifinity
+ * server.
+ * 
+ * @author Rick-Rainer Ludwig
+ * 
+ */
+public class PasswordStoreDatabaseMigrator {
+
+	private static boolean migrate = false;
+	private static boolean drop = false;
+
+	private static void showUsage() {
+		System.out.println("Usage: " + PasswordStoreDatabaseMigrator.class.getSimpleName()
+				+ " (--drop | --migrate)");
+	}
+
+	public static void main(String[] args) throws Exception {
+		if (args.length == 0) {
+			showUsage();
+			return;
+		}
+		for (String arg : args) {
+			if ("--drop".equals(arg)) {
+				drop = true;
+			} else if ("--migrate".equals(arg)) {
+				migrate = true;
+			}
+		}
+		try (GenesisController migrator = new GenesisController()) {
+			if (drop) {
+				migrator.dropAll();
+			}
+			if (migrate) {
+				migrator.transform();
+			}
+		}
+	}
+}
