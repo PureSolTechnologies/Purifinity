5265279c3a8d9a94a880c5b80f0386c0d3ab6ee3
Rick-Rainer Ludwig <ludwig@puresol-technologies.com>
2015-01-26 19:51:10 +0100

    Removed JSF. UI is implemented in HTML5, CSS and JavaScript with Bootstrap and AngularJS.

---
+++ server/global/test/src/test/java/com/puresoltechnologies/purifinity/server/global/test/AbstractFullServerClientTest.java
@@ -0,0 +1,8 @@
+package com.puresoltechnologies.purifinity.server.global.test;
+
+import com.puresoltechnologies.purifinity.wildfly.test.AbstractGlobalClientTest;
+
+public abstract class AbstractFullServerClientTest extends
+		AbstractGlobalClientTest {
+
+}
--- server/global/test/src/test/java/com/puresoltechnologies/purifinity/server/test/AbstractFullServerClientTest.java
+++
@@ -1,8 +0,0 @@
-package com.puresoltechnologies.purifinity.server.test;
-
-import com.puresoltechnologies.purifinity.wildfly.test.AbstractGlobalClientTest;
-
-public abstract class AbstractFullServerClientTest extends
-		AbstractGlobalClientTest {
-
-}
