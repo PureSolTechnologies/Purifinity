d0d99738834ac2a3ad8fe13f69f4ec1d57624b7f
Rick-Rainer Ludwig <ludwig@puresol-technologies.com>
2015-02-14 00:16:33 +0100

    Fixed all tests. They run locally, but the CI is not stable, yet.

--- server/plugins/java7/ejb/src/test/java/com/puresoltechnologies/purifinity/framework/lang/java7/SourceCodeAnalysisDistributionIT.java
+++ server/plugins/java7/ejb/src/test/java/com/puresoltechnologies/purifinity/framework/lang/java7/SourceCodeAnalysisDistributionIT.java
@@ -30,61 +30,61 @@ import com.puresoltechnologies.purifinity.server.plugin.java7.grammar.JavaGramma
  */
 public class SourceCodeAnalysisDistributionIT {
 
-	private static final String INSTALL_DIRECTORY = "/home/ludwig/JavaSource";
+    private static final String INSTALL_DIRECTORY = "/home/ludwig/JavaSource";
 
-	private static final File file = new File(
-			"j2se/src/share/classes/sun/nio/cs/ext/EUC_TW.java");
+    private static final File file = new File(
+	    "j2se/src/share/classes/sun/nio/cs/ext/EUC_TW.java");
 
-	private static File directory = new File(System.getProperty("user.dir"));
+    private static File directory = new File(System.getProperty("user.dir"));
 
-	private static final Grammar javaGrammar = JavaGrammar.getInstance();
+    private static final Grammar javaGrammar = JavaGrammar.getInstance();
 
-	@BeforeClass
-	public static void initialize() {
-		directory = new File(System.getProperty("user.dir"));
-		System.out.println(directory.toString());
-		assertEquals("lib", directory.getName());
-		directory = directory.getParentFile();
-		assertEquals("java7", directory.getName());
-		directory = directory.getParentFile();
-		assertEquals("lang", directory.getName());
-		directory = directory.getParentFile();
-		assertEquals("framework", directory.getName());
+    @BeforeClass
+    public static void initialize() {
+	directory = new File(System.getProperty("user.dir"));
+	System.out.println(directory.toString());
+	assertEquals("ejb", directory.getName());
+	directory = directory.getParentFile();
+	assertEquals("java7", directory.getName());
+	directory = directory.getParentFile();
+	assertEquals("plugins", directory.getName());
+	directory = directory.getParentFile();
+	assertEquals("server", directory.getName());
+    }
+
+    @Test
+    public void testJavaOnSourceCodeAnalysisCode() throws IOException {
+	List<String> includeFiles = new ArrayList<String>();
+	includeFiles.add("*.java");
+	List<String> includeDirectories = new ArrayList<String>();
+	List<String> excludeFiles = new ArrayList<String>();
+	excludeFiles.add("*");
+	List<String> excludeDirectories = new ArrayList<String>();
+	excludeDirectories.add(".*");
+	excludeDirectories.add("target");
+	FileSearchConfiguration configuration = new FileSearchConfiguration(
+		includeDirectories, excludeDirectories, includeFiles,
+		excludeFiles, true);
+	FileTree fileTree = FileSearch.getFileTree(directory, configuration);
+	for (FileTree fileNode : fileTree) {
+	    File file = fileNode.getPathFile(true);
+	    if (file.isFile()) {
+		assertTrue(parseFile(file));
+	    }
 	}
+    }
 
-	@Test
-	public void testJavaOnSourceCodeAnalysisCode() throws IOException {
-		List<String> includeFiles = new ArrayList<String>();
-		includeFiles.add("*.java");
-		List<String> includeDirectories = new ArrayList<String>();
-		List<String> excludeFiles = new ArrayList<String>();
-		excludeFiles.add("*");
-		List<String> excludeDirectories = new ArrayList<String>();
-		excludeDirectories.add(".*");
-		excludeDirectories.add("target");
-		FileSearchConfiguration configuration = new FileSearchConfiguration(
-				includeDirectories, excludeDirectories, includeFiles,
-				excludeFiles, true);
-		FileTree fileTree = FileSearch.getFileTree(directory, configuration);
-		for (FileTree fileNode : fileTree) {
-			File file = fileNode.getPathFile(true);
-			if (file.isFile()) {
-				assertTrue(parseFile(file));
-			}
-		}
+    private static boolean parseFile(File file) throws IOException {
+	try {
+	    System.out.println(file);
+	    Java java = new Java();
+	    CodeAnalyzer analyser = java.createAnalyser(new SourceFileLocation(
+		    file.getParentFile(), file.getName()));
+	    analyser.analyze();
+	    return true;
+	} catch (AnalyzerException e) {
+	    e.printStackTrace();
+	    return false;
 	}
-
-	private static boolean parseFile(File file) throws IOException {
-		try {
-			System.out.println(file);
-			Java java = new Java();
-			CodeAnalyzer analyser = java.createAnalyser(new SourceFileLocation(
-					file.getParentFile(), file.getName()));
-			analyser.analyze();
-			return true;
-		} catch (AnalyzerException e) {
-			e.printStackTrace();
-			return false;
-		}
-	}
+    }
 }
