54aa3ea1ee23df61d83e65484f41e7797bc0d19b
Rick-Rainer Ludwig <ludwig@puresol-technologies.com>
2015-05-14 12:03:31 +0200

    Grunt support into Maven
    Introduce configuration framework

---
+++ webui/webui/src/main/webapp/js/configuration-ui.js
@@ -0,0 +1,47 @@
+var configurationUIModule = angular.module("configurationUIModule", [ "preferencesManagerModule" ]);
+configurationUIModule.directive("configurationComponent", configurationComponent);
+
+function configurationComponent() {
+	return {
+		restrict: "E",
+		scope: {
+			configurationTreeData: '=ngModel'
+		},
+		controller : configurationComponentCtrl,
+		templateUrl: "js/configurationComponent.html"
+	};
+}
+
+function configurationComponentCtrl($scope) {
+	$scope.path = [];
+	$scope.path.push($scope.configurationTreeData);
+	$scope.currentFolder = $scope.configurationTreeData;
+	$scope.chdir = function(dir) {
+		if  (dir == "..") {
+			if ($scope.path.length > 1) {
+				$scope.path.pop();
+				$scope.currentFolder = $scope.path[$scope.path.length - 1];
+			} 
+			return;
+		}
+		for (key in $scope.currentFolder.children) {
+			if ($scope.currentFolder.children[key].content == dir) {
+				var newFolder = $scope.currentFolder.children[key];
+				$scope.path.push(newFolder);
+				$scope.currentFolder = newFolder;
+				return;
+			}
+		}
+	}
+	$scope.setDir = function(dir) {
+		while (($scope.path.length > 1) && ($scope.path[$scope.path.length - 1] !== dir)) {
+			$scope.path.pop();
+		}
+		$scope.currentFolder = $scope.path[$scope.path.length - 1];
+	}
+	$scope.$watch('configurationTreeData', function(newValue, oldValue) {
+		$scope.path = [];
+		$scope.path.push($scope.configurationTreeData);
+		$scope.currentFolder = $scope.configurationTreeData;		
+	});
+};
--- webui/webui/src/main/webapp/js/preferences-manager.js
+++ webui/webui/src/main/webapp/js/preferences-manager.js
@@ -4,9 +4,31 @@
  */
 var preferencesManagerModule = angular.module("preferencesManagerModule", [ "purifinityServer" ]);
 preferencesManagerModule.factory('preferencesManager', [
-		'purifinityServerConnector', preferencesManager ]);
+    'purifinityServerConnector', preferencesManager ]);
+preferencesManagerModule.controller('systemSettingsCtrl', systemSettingsCtrl);
 
 function preferencesManager(purifinityServerConnector) {
-	var preferencesManager = {};
-	return preferencesManager;
+    var preferencesManager = {};
+    preferencesManager.getSystemParameters = function(success, error) {
+	return purifinityServerConnector.get(
+	    '/purifinityserver/rest/preferences/system',
+	    success, 
+	    error
+	);
+    };
+    return preferencesManager;
+}
+
+function systemSettingsCtrl($scope, preferencesManager) {
+    $scope.systemSettingsTree = {
+	children: []
+    };
+    preferencesManager.getSystemParameters(
+	function (data, status) {
+	    for (var i = 0; i < data.length; i++) {
+		var parameter = data[i];
+	    }
+	},
+	function (data, status, error) {}
+    );
 }
