1eda60c418f523df3d79ce14208f30605ecfe8d9
Rick-Rainer Ludwig <ludwig@puresol-technologies.com>
2015-05-29 22:04:44 +0200

    Styling of configuration UI and UI in general.

--- webui/webui/src/main/typescript/server/ProjectManagerModule.ts
+++ webui/webui/src/main/typescript/server/ProjectManagerModule.ts
@@ -2,7 +2,7 @@
  * This JavaScript files contains Angular JS functionality to be added to an
  * application to handle projects for Purifinity.
  */
-var projectManagerModule: angular.IModule = angular.module("projectManagerModule", ["purifinityServerModule"]);
+var projectManagerModule: angular.IModule = angular.module("projectManagerModule", ["purifinityServerModule", "preferencesManagerModule"]);
 
 projectManagerModule.factory('projectManager', ['purifinityServerConnector',
     function(purifinityServerConnector) {
@@ -25,14 +25,6 @@ projectManagerModule.controller("projectListCtrl", function($scope, projectManag
             function(data, status, error) { }
             );
     }
-});
-projectManagerModule.controller("projectsCtrl", function($scope, $routeParams, baseURL) {
-    $scope.projectId = $routeParams['project'];
-    $scope.runId = $routeParams['run'];
-    $scope.mode = $routeParams['mode'];
-    $scope.isSelected = function(id) {
-        return $scope.projectId == id;
-    };
 });
 projectManagerModule.controller("projectSettingsCtrl", function($scope, $modal, $log, projectManager) {
     $scope.projects = {};
@@ -116,12 +108,87 @@ projectManagerModule.controller("createProjectCtrl", function($scope, $location,
     };
 });
 
-projectManagerModule.controller("editProjectCtrl", function($scope, $location, $rootScope, projectManager) {
-    $scope.items = {};
-    $scope.ok = function() {
-        $location.path("/projects");
+projectManagerModule.controller("editProjectCtrl", function($scope, $location, $rootScope, $routeParams, projectManager, pluginManager, preferencesManager) {
+    $scope.projectId = $routeParams['project'];
+    $scope.items = {
+        id: "",
+        name: "",
+        description: "",
+        directoryIncludes: "",
+        directoryExcludes: "",
+        fileIncludes: "",
+        fileExcludes: "",
+        ignoreHidden: true,
+        repositoryId: "",
+        repositoryProperties: {}
     };
-    $scope.cancel = function() {
-        $location.path("/projects");
-    };
+    $scope.repositories = undefined;
+    var pluginSettings: ConfigurationComponentData = new ConfigurationComponentData("Plug-ins", PreferencesGroup.PLUGIN_PROJECT);
+    $scope.pluginSettings = pluginSettings;
+    projectManager.getRepositoryTypes(
+        function(data, status) { $scope.repositories = data }, //
+        function(data, status, error) { });
+    $scope.analyzers = {};
+    $scope.evaluators = {};
+    $scope.repositories = {};
+    var analyzersNode: ConfigurationComponentTree = new ConfigurationComponentTree(pluginSettings.root, "Analyzers");
+    pluginSettings.root.addChild(analyzersNode);
+    var evaluatorsNode: ConfigurationComponentTree = new ConfigurationComponentTree(pluginSettings.root, "Evaluators");
+    pluginSettings.root.addChild(evaluatorsNode);
+    var repositoriesNode: ConfigurationComponentTree = new ConfigurationComponentTree(pluginSettings.root, "Repositories");
+    pluginSettings.root.addChild(repositoriesNode);
+    pluginManager.getAnalyzers(
+        function(data, status) {
+            $scope.analyzers = data;
+            for (var i = 0; i < data.length; i++) {
+                var plugin = data[i];
+                var analyzerNode: ConfigurationComponentTree = new ConfigurationComponentTree(analyzersNode, plugin.name);
+                analyzersNode.addChild(analyzerNode);
+                addPluginConfiguration(preferencesManager, analyzerNode, plugin.id);
+            }
+        },
+        function(data, status, error) { }
+        );
+    pluginManager.getEvaluators(
+        function(data, status) {
+            $scope.evaluators = data;
+            for (var i = 0; i < data.length; i++) {
+                var plugin = data[i];
+                var evaluatorNode: ConfigurationComponentTree = new ConfigurationComponentTree(evaluatorsNode, plugin.name);
+                evaluatorsNode.addChild(evaluatorNode);
+                addPluginConfiguration(preferencesManager, evaluatorNode, plugin.id);
+            }
+        },
+        function(data, status, error) { }
+        );
+    pluginManager.getRepositoryTypes(
+        function(data, status) {
+            $scope.repositories = data;
+            for (var i = 0; i < data.length; i++) {
+                var plugin = data[i];
+                var repositoryNode: ConfigurationComponentTree = new ConfigurationComponentTree(repositoriesNode, plugin.name);
+                repositoriesNode.addChild(repositoryNode);
+                addPluginConfiguration(preferencesManager, repositoryNode, plugin.id);
+            }
+        },
+        function(data, status, error) { }
+        );
+    $scope.$watch('items.repositoryId', function(oldValue, newValue) {
+        var key;
+        for (key in $scope.repositories) {
+            var repository = $scope.repositories[key];
+            if (repository.id == $scope.items.repositoryId) {
+                $scope.repositoryProperties = {};
+                var name;
+                for (name in repository.parameters) {
+                    $scope.repositoryProperties[name] = repository.parameters[name];
+                    $scope.repositoryProperties[name].uiName = name;
+                }
+            }
+        }
+    });
+    $scope.ok =    function() {
+        $location.path("/projects");};
+    $scope.cancel =    function() {
+        $location.path("/projects");};
 });
