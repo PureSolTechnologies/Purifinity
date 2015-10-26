06b34018978bcfef99b84ff52ff9100b81286c88
Rick-Rainer Ludwig <ludwig@puresol-technologies.com>
2015-10-26 22:24:51 +0100

    Removed tests for Neo4j and HGraph. HGraph sources were moved into own HGraph GIT repository.

--- server/database/hgraph/src/main/java/com/puresoltechnologies/purifinity/server/database/hgraph/HGraphEdge.java
+++
@@ -1,7 +0,0 @@
-package com.puresoltechnologies.purifinity.server.database.hgraph;
-
-import com.tinkerpop.blueprints.Edge;
-
-public interface HGraphEdge extends Edge {
-
-}
--- server/database/hgraph/src/main/java/com/puresoltechnologies/purifinity/server/database/hgraph/tx/OperationType.java
+++
@@ -1,7 +0,0 @@
-package com.puresoltechnologies.purifinity.server.database.hgraph.tx;
-
-public enum OperationType {
-
-    PUT, DELETE;
-
-}
