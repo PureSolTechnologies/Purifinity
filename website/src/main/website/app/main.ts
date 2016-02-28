d2db109cbbc7f7ce8803c24d1b20006f9b879184
Rick-Rainer Ludwig <ludwig@puresol-technologies.com>
2016-02-28 21:41:55 +0100

    Fixed compilation issues for Angular2.

--- website/src/main/website/app/main.ts
+++ website/src/main/website/app/main.ts
@@ -1,3 +1,5 @@
+///<reference path="../node_modules/angular2/typings/browser.d.ts"/>
+
 import {provide} from 'angular2/core';
 import {bootstrap}    from 'angular2/platform/browser'
 import {AppComponent} from './app.component'
