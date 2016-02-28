ebd3d1bee3bfcd1320acbb0b29ede6f0d60bf3c5
Rick-Rainer Ludwig <ludwig@puresol-technologies.com>
2016-02-28 20:50:40 +0100

    Feature #341 Create distribution package again with DuctileDB

---
+++ website/src/main/website/app/commons/purifinity.component.ts
@@ -0,0 +1,8 @@
+import {Component} from 'angular2/core';
+
+@Component({
+	selector: 'purifinity',
+	template: `<a href="http://purifinity.com"><span class="purifinity-logo">Purifinity <ng-content></ng-content></span></a>`
+})
+export class PurifinityComponent {
+}
