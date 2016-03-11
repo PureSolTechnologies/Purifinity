470eeb6ac44f97268518ddf46849678cd7df1480
Rick-Rainer Ludwig <ludwig@puresol-technologies.com>
2016-03-11 22:22:22 +0100

    Improvement #283 Introduce AngularJS 2.0
    Information page was re-activated.

--- webui/webui/src/main/webui/app/information.component.ts
+++ webui/webui/src/main/webui/app/information.component.ts
@@ -1,10 +1,17 @@
 import {Component} from 'angular2/core';
 import {ROUTER_DIRECTIVES} from 'angular2/router';
 
+import {SystemHealthComponent} from './components/monitor/system-health.component';
+import {LoggedInUsersComponent} from './components/monitor/logged-in-users.component';
+import {RunningJobsComponent} from './components/monitor/running-jobs.component';
+
 @Component({
     selector: 'information',
     directives: [
-        ROUTER_DIRECTIVES
+        ROUTER_DIRECTIVES,
+        SystemHealthComponent,
+        LoggedInUsersComponent,
+        RunningJobsComponent
     ],
     templateUrl: '../html/information.html'
 })
