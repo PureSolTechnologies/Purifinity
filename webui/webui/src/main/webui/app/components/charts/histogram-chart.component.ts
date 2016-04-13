018088f4ce412fec6ee0e5f6f386d24ba13d095f
Rick-Rainer Ludwig <ludwig@puresol-technologies.com>
2016-04-13 20:12:16 +0200

    Improvement #283 Introduce AngularJS 2.0
    Historgram chart is working again.

--- webui/webui/src/main/webui/app/components/charts/histogram-chart.component.ts
+++ webui/webui/src/main/webui/app/components/charts/histogram-chart.component.ts
@@ -1,4 +1,8 @@
-import {Component} from 'angular2/core';
+import {Component, ElementRef, Input} from 'angular2/core';
+
+import {ParetoDatum} from '../../commons/charts/ParetoDatum';
+import {HtmlElementPrinter} from '../../commons/HtmlElementPrinter';
+import {ChartExport} from '../../commons/ChartExport';
 
 @Component({
     selector: 'histogram-chart',
@@ -23,4 +27,146 @@ import {Component} from 'angular2/core';
 </div>`
 })
 export class HistogramChartComponent {
+
+    @Input()
+    private data: any;
+
+    private svg: d3.Selection<any>;
+    private margin: number;
+
+    constructor(private element: ElementRef) { }
+
+    ngOnInit() {
+        this.svg = d3.select(this.element.nativeElement)
+            .append("svg")
+            .attr("class", "chart")
+            .style("width", "100%");
+
+        this.margin = parseInt(this.element.nativeElement.style.margin, 10) || 30;
+    }
+
+    // watch for data changes and re-render
+    ngOnChanges(changes) {
+        if (this.svg) {
+            this.render();
+        }
+    }
+
+    // Browser onresize event
+    onResize(event): void {
+        this.render();
+    };
+
+    render(): void {
+                        // remove all previous items before render
+                        this.svg.selectAll("*").remove();
+                        if (!this.data) {
+                            return;
+                        }
+                        // setup variables
+                        let node: any = d3.select(this.element.nativeElement).node();
+                        var width = node.parentNode.offsetWidth - 2 * this.margin;
+                        if (width <= 0) {
+                            return;
+                        }
+                        // calculate the height
+                        var height = Math.round(width / 2) - 2 * this.margin;
+                        // bins and bars
+                        var binCount = Math.floor(Math.sqrt(this.data.length));
+                        var barWidth = width / binCount;
+
+                        var dataMax = d3.max(this.data, function(d:any) {
+                            return d.value;
+                        });
+                        var dataMin = d3.min(this.data, function(d:any) {
+                            return d.value;
+                        });
+
+                        var binData = new Array(binCount);
+                        var bins = new Array(binCount);
+                        for (var i = 0; i < binData.length; i++) {
+                            binData[i] = 0;
+                            bins[i] = String((Math.round((dataMin + (dataMax - dataMin) / binCount * (i + 0.5)) * 100) / 100).toFixed(2));
+                        }
+                        for (var key in this.data) {
+                            var value = this.data[key].value;
+                            var binNum = Math.round((value - dataMin) / (dataMax - dataMin) * binCount);
+                            binData[binNum < binCount ? binNum : binCount - 1]++;
+                        }
+
+                        var max = d3.max(binData, function(d) {
+                            return d;
+                        });
+                        var min = d3.min(binData, function(d) {
+                            return d;
+                        });
+
+                        var color = d3.scale.linear<string,number>()
+                            .domain([min, (max - min) / 2, max])
+                            .range(["red", "yellow", "green"]);
+                        // our yScale
+                        var xScale = d3.scale.ordinal()
+                            .domain(bins)
+                            .rangeBands([0, width]);
+                        var yScale = d3.scale.linear()
+                            .domain([0, max])
+                            .range([0, height]);
+
+                        // set the height based on the calculations above
+                        var chart = this.svg
+                            .attr("width", width + 2 * this.margin)
+                            .attr("height", height + 2 * this.margin)
+                            .append("g")
+                            .attr("transform", "translate(" + this.margin + "," + this.margin + ")");
+
+                        var xAxis = d3.svg.axis()
+                            .scale(xScale)
+                            .orient("bottom");
+                        var yAxis = d3.svg.axis()
+                            .scale(yScale)
+                            .orient("left");
+                        chart
+                            .append("g")
+                            .attr("class", "x axis")
+                            .attr("transform", "translate(0," + height + ")")
+                            .call(xAxis);
+                        chart
+                            .append("g")
+                            .attr("class", "y axis")
+                            .call(yAxis);
+
+                        //create the rectangles for the bar chart
+                        chart.selectAll("rect")
+                            .data(binData)
+                            .enter()
+                            .append("rect")
+                            .on("click", function(d, i) {
+                            return this.onClick({ item: d });
+                        })
+                            .attr("height", 0)
+                            .attr("width", barWidth)
+                            .attr("x", function(d, i) {
+                            return i * barWidth;
+                        })
+                            .attr("y", height)
+                            .attr("fill", function(d) { return color(d); })
+                            .transition()
+                            .duration(1000)
+                            .attr("y", function(d) {
+                            return height - yScale(d);
+                        })
+                            .attr("height", function(d) {
+                            return yScale(d);
+                        });
+    }
+
+    printChart() {
+        let printer: HtmlElementPrinter = new HtmlElementPrinter(this.svg.node());
+        printer.print();
+    }
+
+    exportChart() {
+        let exporter: ChartExport = new ChartExport(this.svg.node());
+        exporter.export();
+    }
 }
