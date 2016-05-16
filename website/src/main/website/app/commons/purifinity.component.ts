8badc64ba23ad22d411a14a9f78fd5714d237d34
Rick-Rainer Ludwig <ludwig@puresol-technologies.com>
2016-05-16 22:36:41 +0200

    Removed common web site elements and use them from commons.website.

--- website/src/main/website/app/commons/purifinity.component.ts
+++
@@ -1,8 +0,0 @@
-import {Component} from 'angular2/core';
-
-@Component({
-	selector: 'purifinity',
-	template: `<a href="http://purifinity.com"><span class="purifinity-logo">Purifinity <ng-content></ng-content></span></a>`
-})
-export class PurifinityComponent {
-}
