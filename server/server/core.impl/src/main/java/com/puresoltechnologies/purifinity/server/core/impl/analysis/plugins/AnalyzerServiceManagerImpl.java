d37e8551690290946d293991cc9dee77e2d40cdf
Rick-Rainer Ludwig <ludwig@puresol-technologies.com>
2016-06-06 00:40:11 +0200

    Improvement #386 Update to WildFly 10.0.0.Final

--- server/server/core.impl/src/main/java/com/puresoltechnologies/purifinity/server/core/impl/analysis/plugins/AnalyzerServiceManagerImpl.java
+++ server/server/core.impl/src/main/java/com/puresoltechnologies/purifinity/server/core/impl/analysis/plugins/AnalyzerServiceManagerImpl.java
@@ -22,81 +22,73 @@ import com.puresoltechnologies.purifinity.server.wildfly.utils.JndiUtils;
 @Singleton
 @EJBFacade
 public class AnalyzerServiceManagerImpl
-		extends
-		AbstractServiceManager<AnalyzerServiceInformation, ProgrammingLanguageAnalyzer>
-		implements AnalyzerServiceManager, AnalyzerServiceManagerRemote {
+	extends AbstractServiceManager<AnalyzerServiceInformation, ProgrammingLanguageAnalyzer>
+	implements AnalyzerServiceManager, AnalyzerServiceManagerRemote {
 
-	@Inject
-	private PreferencesStore preferencesStore;
+    @Inject
+    private PreferencesStore preferencesStore;
 
-	private final Map<String, Boolean> analyzerActivations = new HashMap<>();
+    private final Map<String, Boolean> analyzerActivations = new HashMap<>();
 
-	public AnalyzerServiceManagerImpl() {
-		super("Analyzer Service Manager");
+    public AnalyzerServiceManagerImpl() {
+	super("Analyzer Service Manager");
+    }
+
+    @Override
+    @Lock(LockType.READ)
+    public ProgrammingLanguageAnalyzer createProxy(String jndi) {
+	ProgrammingLanguageAnalyzer analyzer = JndiUtils.createRemoteEJBInstance(ProgrammingLanguageAnalyzer.class,
+		jndi);
+	AnalyzerServiceInformation information = findByName(analyzer.getName(), analyzer.getVersion());
+	for (ConfigurationParameter<?> configurationParameter : analyzer.getConfigurationParameters()) {
+	    PreferencesValue<?> value = preferencesStore.getPluginDefaultPreference(information.getId(),
+		    configurationParameter);
+	    if (value != null) {
+		analyzer.setConfigurationParameter(configurationParameter, value.getValue());
+	    }
 	}
+	return analyzer;
+    }
 
-	@Override
-	@Lock(LockType.READ)
-	public ProgrammingLanguageAnalyzer createProxy(String jndi) {
-		ProgrammingLanguageAnalyzer analyzer = JndiUtils
-				.createRemoteEJBInstance(ProgrammingLanguageAnalyzer.class,
-						jndi);
-		AnalyzerServiceInformation information = findByName(analyzer.getName(),
-				analyzer.getVersion());
-		for (ConfigurationParameter<?> configurationParameter : analyzer
-				.getConfigurationParameters()) {
-			PreferencesValue<?> value = preferencesStore
-					.getPluginDefaultPreference(information.getId(),
-							configurationParameter);
-			if (value != null) {
-				analyzer.setConfigurationParameter(configurationParameter,
-						value.getValue());
-			}
-		}
-		return analyzer;
+    @Override
+    @Lock(LockType.READ)
+    public AnalyzerServiceInformation findByName(String languageName, String languageVersion) {
+	for (AnalyzerServiceInformation analyzerServiceInformation : getServices()) {
+	    if (analyzerServiceInformation.getName().equals(languageName)
+		    && analyzerServiceInformation.getVersion().equals(languageVersion)) {
+		return analyzerServiceInformation;
+	    }
 	}
+	return null;
+    }
 
-	@Override
-	@Lock(LockType.READ)
-	public AnalyzerServiceInformation findByName(String languageName,
-			String languageVersion) {
-		for (AnalyzerServiceInformation analyzerServiceInformation : getServices()) {
-			if (analyzerServiceInformation.getName().equals(languageName)
-					&& analyzerServiceInformation.getVersion().equals(
-							languageVersion)) {
-				return analyzerServiceInformation;
-			}
-		}
-		return null;
+    @Override
+    @Lock(LockType.READ)
+    public boolean isActive(String analyzerId) {
+	Boolean active = analyzerActivations.get(analyzerId);
+	if (active != null) {
+	    return active;
 	}
+	active = preferencesStore.isServiceActive(analyzerId);
+	analyzerActivations.put(analyzerId, active);
+	return active;
+    }
 
-	@Override
-	@Lock(LockType.READ)
-	public boolean isActive(String analyzerId) {
-		Boolean active = analyzerActivations.get(analyzerId);
-		if (active != null) {
-			return active;
-		}
-		active = preferencesStore.isServiceActive(analyzerId);
-		analyzerActivations.put(analyzerId, active);
-		return active;
-	}
+    @Override
+    @Lock(LockType.WRITE)
+    public void setActive(String analyzerId, boolean active) {
+	preferencesStore.setServiceActive(analyzerId, active);
+	analyzerActivations.put(analyzerId, active);
+    }
 
-	@Override
-	@Lock(LockType.READ)
-	public void setActive(String analyzerId, boolean active) {
-		preferencesStore.setServiceActive(analyzerId, active);
-		analyzerActivations.put(analyzerId, active);
+    @Override
+    @Lock(LockType.READ)
+    public ProgrammingLanguageAnalyzer getInstanceById(String analyzerId) {
+	for (AnalyzerServiceInformation analyzer : getServices()) {
+	    if (analyzer.getId().equals(analyzerId)) {
+		return createProxy(analyzer.getJndiName());
+	    }
 	}
-
-	@Override
-	@Lock(LockType.READ)
-	public ProgrammingLanguageAnalyzer getInstanceById(String analyzerId) {
-		for (AnalyzerServiceInformation analyzer : getServices()) {
-			if (analyzer.getId().equals(analyzerId)) {
-				return createProxy(analyzer.getJndiName());
-			}
-		}
-		return null;
-	}
+	return null;
+    }
 }
--- server/server/core.impl/src/main/java/com/puresoltechnologies/purifinity/server/core/impl/evaluation/plugins/EvaluatorServiceManagerImpl.java
+++ server/server/core.impl/src/main/java/com/puresoltechnologies/purifinity/server/core/impl/evaluation/plugins/EvaluatorServiceManagerImpl.java
@@ -32,155 +32,145 @@ import com.puresoltechnologies.purifinity.server.wildfly.utils.JndiUtils;
 
 @Singleton
 @EJBFacade
-public class EvaluatorServiceManagerImpl extends
-		AbstractServiceManager<EvaluatorServiceInformation, Evaluator>
-		implements EvaluatorServiceManager, EvaluatorServiceManagerRemote {
+public class EvaluatorServiceManagerImpl extends AbstractServiceManager<EvaluatorServiceInformation, Evaluator>
+	implements EvaluatorServiceManager, EvaluatorServiceManagerRemote {
 
-	@Inject
-	private Logger logger;
+    @Inject
+    private Logger logger;
 
-	@Inject
-	private PreferencesStore preferencesStore;
+    @Inject
+    private PreferencesStore preferencesStore;
 
-	@Inject
-	private EvaluatorStoreUtils evaluatorStoreCassandraUtils;
+    @Inject
+    private EvaluatorStoreUtils evaluatorStoreCassandraUtils;
 
-	private final Map<String, Boolean> analyzerActivations = new HashMap<>();
+    private final Map<String, Boolean> analyzerActivations = new HashMap<>();
 
-	public EvaluatorServiceManagerImpl() {
-		super("Evaluator Service Manager");
+    public EvaluatorServiceManagerImpl() {
+	super("Evaluator Service Manager");
+    }
+
+    @Override
+    @Lock(LockType.WRITE)
+    public void registerService(PluginInformation pluginInformation, String jndiName,
+	    EvaluatorServiceInformation serviceInformation) {
+	super.registerService(pluginInformation, jndiName, serviceInformation);
+	addInformationToDatabase(pluginInformation, serviceInformation);
+    }
+
+    private void addInformationToDatabase(PluginInformation pluginInformation,
+	    EvaluatorServiceInformation serviceInformation) {
+	evaluatorStoreCassandraUtils.registerPluginInformation(pluginInformation, serviceInformation);
+    }
+
+    @Override
+    @Lock(LockType.READ)
+    public Evaluator createProxy(String jndi) {
+	Evaluator evaluator = JndiUtils.createRemoteEJBInstance(Evaluator.class, jndi);
+	EvaluatorInformation information = evaluator.getInformation();
+	for (ConfigurationParameter<?> configurationParameter : evaluator.getConfigurationParameters()) {
+	    PreferencesValue<?> value = preferencesStore.getPluginDefaultPreference(information.getId(),
+		    configurationParameter);
+	    if (value != null) {
+		evaluator.setConfigurationParameter(configurationParameter, value.getValue());
+	    }
 	}
+	return evaluator;
+    }
 
-	@Override
-	@Lock(LockType.WRITE)
-	public void registerService(PluginInformation pluginInformation,
-			String jndiName, EvaluatorServiceInformation serviceInformation) {
-		super.registerService(pluginInformation, jndiName, serviceInformation);
-		addInformationToDatabase(pluginInformation, serviceInformation);
+    @Override
+    @Lock(LockType.READ)
+    public Evaluator createInstanceById(String evaluatorId) {
+	for (EvaluatorServiceInformation evaluator : getServices()) {
+	    if (evaluator.getId().equals(evaluatorId)) {
+		return createProxy(evaluator.getJndiName());
+	    }
 	}
+	return null;
+    }
 
-	private void addInformationToDatabase(PluginInformation pluginInformation,
-			EvaluatorServiceInformation serviceInformation) {
-		evaluatorStoreCassandraUtils.registerPluginInformation(
-				pluginInformation, serviceInformation);
-	}
-
-	@Override
-	@Lock(LockType.READ)
-	public Evaluator createProxy(String jndi) {
-		Evaluator evaluator = JndiUtils.createRemoteEJBInstance(
-				Evaluator.class, jndi);
-		EvaluatorInformation information = evaluator.getInformation();
-		for (ConfigurationParameter<?> configurationParameter : evaluator
-				.getConfigurationParameters()) {
-			PreferencesValue<?> value = preferencesStore
-					.getPluginDefaultPreference(information.getId(),
-							configurationParameter);
-			if (value != null) {
-				evaluator.setConfigurationParameter(configurationParameter,
-						value.getValue());
-			}
-		}
+    @Override
+    @Lock(LockType.READ)
+    public EvaluatorServiceInformation getEvaluatorPluginInformation(String evaluatorId) {
+	for (EvaluatorServiceInformation evaluator : getServices()) {
+	    if (evaluator.getId().equals(evaluatorId)) {
 		return evaluator;
+	    }
 	}
+	return null;
+    }
 
-	@Override
-	@Lock(LockType.READ)
-	public Evaluator createInstanceById(String evaluatorId) {
-		for (EvaluatorServiceInformation evaluator : getServices()) {
-			if (evaluator.getId().equals(evaluatorId)) {
-				return createProxy(evaluator.getJndiName());
-			}
+    @Override
+    @Lock(LockType.READ)
+    public List<EvaluatorServiceInformation> getServicesSortedByDependency() {
+	return sortEvaluators(getServices());
+    }
+
+    private List<EvaluatorServiceInformation> sortEvaluators(Collection<EvaluatorServiceInformation> evaluators) {
+	List<EvaluatorServiceInformation> sortedEvaluators = new ArrayList<>();
+	Set<String> foundDependencies = new HashSet<>();
+	boolean changed = true;
+	while ((!evaluators.isEmpty()) && (changed)) {
+	    changed = false;
+	    Iterator<EvaluatorServiceInformation> evaluatorIterator = evaluators.iterator();
+	    while (evaluatorIterator.hasNext()) {
+		EvaluatorServiceInformation evaluator = evaluatorIterator.next();
+		boolean ok = true;
+		for (String dependency : evaluator.getDependencies()) {
+		    if (!foundDependencies.contains(dependency)) {
+			ok = false;
+			break;
+		    }
 		}
-		return null;
-	}
-
-	@Override
-	@Lock(LockType.READ)
-	public EvaluatorServiceInformation getEvaluatorPluginInformation(
-			String evaluatorId) {
-		for (EvaluatorServiceInformation evaluator : getServices()) {
-			if (evaluator.getId().equals(evaluatorId)) {
-				return evaluator;
-			}
+		if (ok) {
+		    sortedEvaluators.add(evaluator);
+		    foundDependencies.add(evaluator.getId());
+		    evaluatorIterator.remove();
+		    changed = true;
 		}
-		return null;
+	    }
 	}
-
-	@Override
-	@Lock(LockType.READ)
-	public List<EvaluatorServiceInformation> getServicesSortedByDependency() {
-		return sortEvaluators(getServices());
-	}
-
-	private List<EvaluatorServiceInformation> sortEvaluators(
-			Collection<EvaluatorServiceInformation> evaluators) {
-		List<EvaluatorServiceInformation> sortedEvaluators = new ArrayList<>();
-		Set<String> foundDependencies = new HashSet<>();
-		boolean changed = true;
-		while ((!evaluators.isEmpty()) && (changed)) {
-			changed = false;
-			Iterator<EvaluatorServiceInformation> evaluatorIterator = evaluators
-					.iterator();
-			while (evaluatorIterator.hasNext()) {
-				EvaluatorServiceInformation evaluator = evaluatorIterator
-						.next();
-				boolean ok = true;
-				for (String dependency : evaluator.getDependencies()) {
-					if (!foundDependencies.contains(dependency)) {
-						ok = false;
-						break;
-					}
-				}
-				if (ok) {
-					sortedEvaluators.add(evaluator);
-					foundDependencies.add(evaluator.getId());
-					evaluatorIterator.remove();
-					changed = true;
-				}
-			}
+	if (logger.isWarnEnabled()) {
+	    StringBuilder builder = new StringBuilder();
+	    for (EvaluatorServiceInformation evaluator : evaluators) {
+		if (builder.length() > 0) {
+		    builder.append(", ");
 		}
-		if (logger.isWarnEnabled()) {
-			StringBuilder builder = new StringBuilder();
-			for (EvaluatorServiceInformation evaluator : evaluators) {
-				if (builder.length() > 0) {
-					builder.append(", ");
-				}
-				builder.append(evaluator.getName());
-			}
-			logger.warn("The following evaluators were not provided due to missing dependencies: "
-					+ builder.toString());
-		}
-		return sortedEvaluators;
+		builder.append(evaluator.getName());
+	    }
+	    logger.warn(
+		    "The following evaluators were not provided due to missing dependencies: " + builder.toString());
 	}
+	return sortedEvaluators;
+    }
 
-	@Override
-	@Lock(LockType.READ)
-	public boolean isActive(String evaluatorId) {
-		Boolean active = analyzerActivations.get(evaluatorId);
-		if (active != null) {
-			return active;
-		}
-		active = preferencesStore.isServiceActive(evaluatorId);
-		analyzerActivations.put(evaluatorId, active);
-		return active;
+    @Override
+    @Lock(LockType.READ)
+    public boolean isActive(String evaluatorId) {
+	Boolean active = analyzerActivations.get(evaluatorId);
+	if (active != null) {
+	    return active;
 	}
+	active = preferencesStore.isServiceActive(evaluatorId);
+	analyzerActivations.put(evaluatorId, active);
+	return active;
+    }
 
-	@Override
-	@Lock(LockType.READ)
-	public void setActive(String evaluatorId, boolean active) {
-		preferencesStore.setServiceActive(evaluatorId, active);
-		analyzerActivations.put(evaluatorId, active);
-	}
+    @Override
+    @Lock(LockType.WRITE)
+    public void setActive(String evaluatorId, boolean active) {
+	preferencesStore.setServiceActive(evaluatorId, active);
+	analyzerActivations.put(evaluatorId, active);
+    }
 
-	@Override
-	@Lock(LockType.READ)
-	public Evaluator getInstanceById(String evaluatorId) {
-		for (EvaluatorServiceInformation evaluator : getServices()) {
-			if (evaluator.getId().equals(evaluatorId)) {
-				return createProxy(evaluator.getJndiName());
-			}
-		}
-		return null;
+    @Override
+    @Lock(LockType.READ)
+    public Evaluator getInstanceById(String evaluatorId) {
+	for (EvaluatorServiceInformation evaluator : getServices()) {
+	    if (evaluator.getId().equals(evaluatorId)) {
+		return createProxy(evaluator.getJndiName());
+	    }
 	}
+	return null;
+    }
 }
