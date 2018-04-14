3fcad263c1fe91290a51a5908f51db30bd47592a
Rick-Rainer Ludwig <ludwig@puresol-technologies.com>
2018-04-14 21:33:04 +0200

    Some refactoring.

--- test/src/test/java/com/puresoltechnologies/debugging/test/AsmTest.java
+++ test/src/test/java/com/puresoltechnologies/debugging/test/AsmTest.java
@@ -13,7 +13,7 @@ import org.objectweb.asm.ClassWriter;
 
 import com.puresoltechnologies.debugging.agent.ByteCodeCassLoader;
 import com.puresoltechnologies.debugging.agent.asm.ClassPrinter;
-import com.puresoltechnologies.debugging.agent.asm.InstrumentClassVisitor;
+import com.puresoltechnologies.debugging.agent.profiler.asm.ProfilerClassVisitor;
 import com.puresoltechnologies.streaming.binary.BinaryOutputStream;
 
 public class AsmTest {
@@ -30,7 +30,7 @@ public class AsmTest {
     public void test2() throws Exception {
 	ClassWriter cw = new ClassWriter(0);
 	ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-	InstrumentClassVisitor cv = new InstrumentClassVisitor(cw, 1,
+	ProfilerClassVisitor cv = new ProfilerClassVisitor(cw, 1,
 		new BinaryOutputStream(outputStream, ByteOrder.LITTLE_ENDIAN));
 	ClassReader cr = new ClassReader(AsmTest.class.getName());
 	cr.accept(cv, 0);
