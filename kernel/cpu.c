6d623ea11ec8defba30fe8e9eb29eb65b5bc50bd
Rick-Rainer Ludwig <ludwig@puresol-technologies.com>
2018-08-12 12:05:15 +0200

    Got kernel thread working incl. stop.

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -4,33 +4,30 @@
  * Taken from: /LinuxKernel/fs/proc/stat.c
  */
 
-//int thread_fn(void* data)
-//{
-//        unsigned long j0, j1;
-//        int delay = 600 * HZ;
-//        j0 = jiffies;
-//        j1 = j0 + delay;
-//
-//        pr_info("In thread1");
-//
-//        while (time_before(jiffies, j1)) {
-//                schedule();
-//        }
-//        return 0;
-//}
-//
-//static struct task_struct *cpu_thread;
+int thread_fn(void* data)
+{
+        while (!kthread_should_stop()) {
+                pr_info("In thread1");
+                ssleep(5);
+        }
+        pr_info("Thread Stopping\n");
+        do_exit(0);
+        return 0;
+}
+
+static struct task_struct *cpu_thread;
+
 void sake_start_cpu_monitor(void)
 {
-//        char our_thread[9] = "sake-cpu";
+        char our_thread[9] = "sake-cpu";
 
         pr_info("Starting CPU monitor...");
-//        pr_info("in init");
-//        cpu_thread = kthread_create(thread_fn, NULL, our_thread);
-//        if ((cpu_thread)) {
-//                pr_info("in if");
-//                wake_up_process(cpu_thread);
-//        }
+        pr_info("in init");
+        cpu_thread = kthread_create(thread_fn, NULL, our_thread);
+        if (cpu_thread) {
+                pr_info("in if");
+                wake_up_process(cpu_thread);
+        }
 
         pr_info("CPU monitor started.");
 }
@@ -38,6 +35,10 @@ void sake_start_cpu_monitor(void)
 void sake_stop_cpu_monitor(void)
 {
         pr_info("Stopping CPU monitor...");
+        if (cpu_thread) {
+                kthread_stop(cpu_thread);
+                pr_info("Thread stopped");
+        }
         pr_info("CPU monitor stopped.");
 }
 
--- kernel/cpu.h
+++ kernel/cpu.h
@@ -6,6 +6,7 @@
 #include <linux/kthread.h>  // for threads
 #include <linux/time.h>   // for using jiffies
 #include <linux/timer.h>
+#include <linux/delay.h>
 
 extern void sake_start_cpu_monitor(void);
 
