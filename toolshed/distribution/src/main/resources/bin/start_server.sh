21dab20653c9ac1b8467f3bd7e00a35e91295066
Rick-Rainer Ludwig <ludwig@puresol-technologies.com>
2018-08-26 23:01:12 +0200

    Work on distribution.

---
+++ toolshed/distribution/src/main/resources/bin/start_server.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Evaluating FAMILITY_HOME
+TOOLSHED_HOME="$( cd "$( dirname $0 )/.." && pwd )"
+
+# JVM arguments...
+JVM_ARGS="-Dtoolshed.home=$TOOLSHED_HOME"
+# Debug arguments 
+#JVM_ARGS="$JVM_ARGS -agentlib:jdwp=transport=dt_socket,server=y,address=8000,suspend=y" 
+
+# Running Famility...
+java $JVM_ARGS -classpath "$TOOLSHED_HOME/lib/*" com.puresoltechnologies.toolshed.server.impl.ToolShedServer server $TOOLSHED_HOME/etc/server.yml
